bbce870889644713aded70ea8510d124fc5c5b39
Chris Dyer <redpony@gmail.com>
2015-03-27 17:17:52 -0400

    init stuff

--- cnn/tests/test_init.cc
+++ cnn/tests/test_init.cc
@@ -4,25 +4,31 @@
 
 #include <vector>
 
+#include "cnn/tests/test_utils.h"
 #include "cnn/tensor.h"
 #include "cnn/saxe_init.h"
-#include "cnn/edges.h"
 
 using namespace std;
 using namespace cnn;
 
+BOOST_GLOBAL_FIXTURE(TestTensorSetup)
+
 BOOST_AUTO_TEST_CASE(EOrthonormalRandom)
 {
-  for (unsigned d = 3; d < 100; d += 5) {
+  for (int d = 4; d < 100; d += 16) {
     Tensor Q = OrthonormalRandom(d, 1.0);
-    BOOST_REQUIRE_EQUAL(size(Q), Dim(d,d));
+    BOOST_REQUIRE_EQUAL(size(Q), Dim({d,d}));
 
     // check that this is actually returning orthogonal matrices
+#if MINERVA_BACKEND
+    Tensor I = Q.Trans() * Q;
+#else
     Tensor I = Q.transpose() * Q;
+#endif
     double eps = 1e-1;
-    for (unsigned i = 0; i < d; ++i)
-      for (unsigned j = 0; j < d; ++j)
-        BOOST_CHECK_CLOSE(I(i,j) + 1., (i == j ? 2. : 1.), eps);
+    for (int i = 0; i < d; ++i)
+      for (int j = 0; j < d; ++j)
+        BOOST_CHECK_CLOSE(t(I,i,j) + 1., (i == j ? 2. : 1.), eps);
   }
 }
 
