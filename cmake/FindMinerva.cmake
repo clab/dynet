3f3ec93c2f7a641a6feed0d7ff150371dc1ef601
Chris Dyer <redpony@gmail.com>
2015-03-30 18:22:20 -0400

    new minerva layout

--- cmake/FindMinerva.cmake
+++ cmake/FindMinerva.cmake
@@ -10,13 +10,7 @@ find_path(MINERVA_INCLUDE_DIR
 
 find_library(MINERVA_LIBRARY
     NAMES minerva
-    PATHS ${MINERVA_PREFIX_PATH}/release/lib)
-
-if(NOT MINERVA_LIBRARY)
-  find_library(MINERVA_LIBRARY
-      NAMES minerva
-      PATHS ${MINERVA_PREFIX_PATH}/debug/lib)
-endif(NOT MINERVA_LIBRARY)
+    PATHS ${MINERVA_PREFIX_PATH}/lib)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Minerva DEFAULT_MSG MINERVA_LIBRARY MINERVA_INCLUDE_DIR)
