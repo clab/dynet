3b1a5b91f5e69d640da37fa00ae0c42a3fca782d
Chris Dyer <redpony@gmail.com>
2015-05-05 23:17:35 -0400

    memory allocator

---
+++ cnn/aligned-mem-pool.h
@@ -0,0 +1,80 @@
+#ifndef CNN_ALIGNED_MEM_POOL_H
+#define CNN_ALIGNED_MEM_POOL_H
+
+#include <cstdlib>
+#include <cstring>
+//#if HAVE_MM_MALLOC
+#include <mm_malloc.h>
+//#endif
+
+namespace cnn {
+
+inline void* cnn_mm_malloc(size_t n, size_t align) {
+//#if HAVE_MM_MALLOC
+  return _mm_malloc(n, align);
+//#else
+//  return std::malloc(n, align);
+//#endif
+}
+
+inline void cnn_mm_free(void* mem) {
+//#if HAVE_MM_MALLOC
+  _mm_free(mem);
+//#else
+//  return std::free(n, align);
+//#endif
+}
+
+// this is used to manage CPU memory for function values and gradients
+template <unsigned AlignedBits>
+class AlignedMemoryPool {
+ public:
+  explicit AlignedMemoryPool(size_t cap) {
+    sys_alloc(cap);
+    zero();
+  }
+  // returns nullptr if OOM
+  void* allocate(size_t n) {
+    auto rounded_n = round_up_align(n);
+    if (rounded_n + used > capacity)
+      return nullptr;
+    void* res = static_cast<char*>(mem) + used;
+    used += rounded_n;
+    return res;
+  }
+  void free() {
+    //std::cerr << "freeing " << used << " bytes\n";
+    used = 0;
+  }
+  void zero_and_free() { zero(); free(); }
+  void free_and_grow_capacity(size_t new_cap = 0) {
+    cnn_mm_free(mem);
+    if (new_cap)
+      sys_alloc(new_cap);
+    else
+      sys_alloc(capacity * 1.5);
+    zero();
+  }
+ private:
+  void sys_alloc(size_t cap) {
+    capacity = round_up_align(cap);
+    mem = cnn_mm_malloc(capacity, 1 << AlignedBits);
+    used = 0;
+  }
+  void zero() {
+    //std::cerr << "zeroing " << (used ? used : capacity) << " bytes\n";
+    std::memset(mem, 0, used ? used : capacity);
+  }
+  inline static size_t round_up_align(size_t n) {
+    if (AlignedBits < 2) return n;
+    auto c = (n & ((1 << AlignedBits) - 1)) > 0 ? 1 : 0;
+    return ((n >> AlignedBits) + c) << AlignedBits;
+  }
+  size_t capacity;
+  size_t used;
+  void* mem;
+};
+
+} // namespace cnn
+
+#endif
