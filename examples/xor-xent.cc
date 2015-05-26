4788f4ef3d0efaefd91b0b47092f54a8bf25d4a5
David Chiang <dchiang@nd.edu>
2015-05-26 14:21:48 -0400

    adding Expression class to make constructing networks simpler. xor examples working.
    Former-commit-id: cf5bc6994c488c01dd5381b220a4e35c5e24c775

--- examples/xor-xent.cc
+++ examples/xor-xent.cc
@@ -1,6 +1,7 @@
 #include "cnn/nodes.h"
 #include "cnn/cnn.h"
 #include "cnn/training.h"
+#include "cnn/expr.h"
 
 #include <boost/archive/text_iarchive.hpp>
 #include <boost/archive/text_oarchive.hpp>
@@ -10,6 +11,7 @@
 
 using namespace std;
 using namespace cnn;
+using namespace cnn::expr;
 
 int main(int argc, char** argv) {
   cnn::Initialize(argc, argv);
@@ -20,42 +22,21 @@ int main(int argc, char** argv) {
   SimpleSGDTrainer sgd(&m);
   //MomentumSGDTrainer sgd(&m);
 
-  Parameters& p_a = *m.add_parameters({1});
-  Parameters& p_b = *m.add_parameters({HIDDEN_SIZE});
-  Parameters& p_W = *m.add_parameters({HIDDEN_SIZE, 2});
-  Parameters& p_V = *m.add_parameters({1, HIDDEN_SIZE});
-
-  // build the graph
   ComputationGraph cg;
 
-  // get symbolic variables corresponding to parameters
-  VariableIndex i_b = cg.add_parameters(&p_b);
-  VariableIndex i_a = cg.add_parameters(&p_a);
-  VariableIndex i_W = cg.add_parameters(&p_W);
-  VariableIndex i_V = cg.add_parameters(&p_V);
+  Expression W = parameter(cg, m.add_parameters({HIDDEN_SIZE, 2}));
+  Expression b = parameter(cg, m.add_parameters({HIDDEN_SIZE}));
+  Expression V = parameter(cg, m.add_parameters({1, HIDDEN_SIZE}));
+  Expression a = parameter(cg, m.add_parameters({1}));
 
   vector<float> x_values(2);  // set x_values to change the inputs to the network
-  VariableIndex i_x = cg.add_input({2}, &x_values);
+  Expression x = input(cg, {2}, &x_values);
   cnn::real y_value;  // set y_value to change the target output
 
-  // two options: MatrixMultiply and Sum, or AffineTransform
-  // these are identical, but AffineTransform may be slightly more efficient
-#if 0
-  VariableIndex i_f = cg.add_function<MatrixMultiply>({i_W, i_x});
-  VariableIndex i_g = cg.add_function<Sum>({i_f, i_b});
-#else
-  VariableIndex i_g = cg.add_function<AffineTransform>({i_b, i_W, i_x});
-#endif
-  VariableIndex i_h = cg.add_function<Tanh>({i_g});
+  Expression h = tanh(W*x + b);
+  Expression y_pred = logistic(V*h + a);
+  Expression loss(&cg, cg.add_function<BinaryLogLoss>({y_pred.i}, &y_value)); // DWC: I didn't understand why second argument is a real* rather than an input
 
-#if 0
-  VariableIndex i_p = cg.add_function<MatrixMultiply>({i_V, i_h});
-  VariableIndex i_uy_pred = cg.add_function<Sum>({i_p, i_a});
-#else
-  VariableIndex i_uy_pred = cg.add_function<AffineTransform>({i_a, i_V, i_h});
-#endif
-  VariableIndex i_y_pred = cg.add_function<LogisticSigmoid>({i_uy_pred});
-  cg.add_function<BinaryLogLoss>({i_y_pred}, &y_value);
   cg.PrintGraphviz();
   //if (argc == 2) {
   //  ifstream in(argv[1]);
