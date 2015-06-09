93a1e71e833fd11e12e657549bb5c96be8e4d1cb
davidweichiang <dchiang@nd.edu>
2015-06-09 14:49:37 -0400

    check for memory allocation

--- cnn/exec.cc
+++ cnn/exec.cc
@@ -35,7 +35,10 @@ const Tensor& SimpleExecutionEngine::incremental_forward() {
     }
     nfxs[last_node_evaluated].d = node->dim;
     nfxs[last_node_evaluated].v = static_cast<float*>(fxs->allocate(node->dim.size() * sizeof(float)));
-    //cerr << "CALLING: " << node->as_string(dummy) << endl;
+    if (nfxs[last_node_evaluated].v == nullptr) {
+      cerr << "out of memory\n";
+      abort();
+    }
     node->forward(xs, nfxs[last_node_evaluated]);
   }
   return nfxs.back();
