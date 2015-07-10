5040a6e45e17027aef469d26cd1777881cc61530
Chris Dyer <redpony@gmail.com>
2015-07-10 14:43:59 -0700

    fix concat cols
    Former-commit-id: b10bb37dc46370028d0c46bff63733284185efb7

--- cnn/nodes.h
+++ cnn/nodes.h
@@ -242,6 +242,7 @@ struct ConcatenateColumns : public Node {
   template <typename T> explicit ConcatenateColumns(const T& a) : Node(a) {}
   std::string as_string(const std::vector<std::string>& arg_names) const override;
   Dim dim_forward(const std::vector<Dim>& xs) const override;
+  size_t aux_storage_size() const override;
   void forward(const std::vector<const Tensor*>& xs, Tensor& fx) const override;
   void backward(const std::vector<const Tensor*>& xs,
                   const Tensor& fx,
