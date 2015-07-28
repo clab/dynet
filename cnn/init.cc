029e1449551df245d650d96f3cd848468934156a
kaisheny <ksyao@hotmail.com>
2015-07-28 00:03:03 -0700

    support using specified random seed number

--- cnn/init.cc
+++ cnn/init.cc
@@ -6,6 +6,10 @@
 #include <random>
 #include <cmath>
 
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+
+
 #if HAVE_CUDA
 #include "cnn/cuda.h"
 #include <device_launch_parameters.h>
@@ -32,9 +36,25 @@ void Initialize(int& argc, char**& argv) {
   kSCALAR_ZERO = (float*) cnn_mm_malloc(1, 256);
   *kSCALAR_ZERO = 0;
 #endif
-  random_device rd;
-//  rndeng = new mt19937(1);
-  rndeng = new mt19937(rd());
+
+  using namespace boost::program_options;
+  variables_map vm;
+  options_description opts("initialization setup");
+  opts.add_options()
+      ("seed,s", value<int>()->default_value(217), "seed number for random number genreation");
+
+  store(parse_command_line(argc, argv, opts), vm);
+
+  if (vm.count("seed")) {
+      int rseed = vm["seed"].as<int>();
+      rndeng = new mt19937(rseed);
+  }
+  else
+  {
+      random_device rd;
+      //  rndeng = new mt19937(1);
+      rndeng = new mt19937(rd());
+  }
   cerr << "Allocating memory...\n";
   fxs = new AlignedMemoryPool<ALIGN>(512*(1<<20));
   dEdfs = new AlignedMemoryPool<ALIGN>(512*(1<<20));
