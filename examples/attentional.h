66ce9e93df6817b99a24962ecc5b83c6665ab607
kaishengyao <kaisheny@KAISHENGLP1.redmond.corp.microsoft.com>
2015-07-29 15:57:37 -0700

    fixing upstream master bug

--- examples/attentional.h
+++ examples/attentional.h
@@ -239,8 +239,13 @@ Expression AttentionalModel<Builder>::add_input(int trg_tok, int t, ComputationG
 	i_e_t = transpose(tanh(i_e_t_input)) * i_va;
 	//WTF(i_e_t);
     } else {
-	i_e_t = transpose(tanh(i_wah_rep + i_uax)) * i_va;
-	//WTF(i_e_t);
+        /// debug
+        Expression before_tanh = i_wah_rep + i_uax;
+        Expression before_transpose = tanh(before_tanh);
+        Expression after_transpose = transpose(before_transpose);
+        Expression times_at = after_transpose * i_va;
+        i_e_t = transpose(tanh(i_wah_rep + i_uax)) * i_va;
+        //WTF(i_e_t);
     }
     Expression i_alpha_t = softmax(i_e_t);
     //WTF(i_alpha_t);
