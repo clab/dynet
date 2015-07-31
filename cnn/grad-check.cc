d7434c6db865282c6c282d596b68034531cae4d6
Chris Dyer <redpony@gmail.com>
2015-07-30 18:27:53 -0700

    lookup table check
    Former-commit-id: 0a51ce509d0b184d86fdb4543c547c291b2e2af3

--- cnn/grad-check.cc
+++ cnn/grad-check.cc
@@ -12,7 +12,7 @@ using namespace std;
 namespace cnn {
 
 void CheckGrad(Model& m, ComputationGraph& g) {
-  float alpha = 1e-4;
+  float alpha = 5e-4;
   float E = as_scalar(g.forward());
   g.backward();
 
@@ -32,17 +32,47 @@ void CheckGrad(Model& m, ComputationGraph& g) {
       float g = (E_right - E_left) / (2 * alpha);
       float f = fabs(g - p.g.v[i]);
       float m = max(fabs(g), fabs(p.g.v[i]));
-      if (m > 0.f) f /= m;
-      if (f > 0.1) { flag = true; cerr << "*** "; }
+      if (f > 0.1) {
+        if (m > 0.f) f /= m;
+        if (f > 0.1) { flag = true; cerr << "***[" << f << "] "; }
+      }
       cerr << p.g.v[i] << ' ' << g << endl;
     }
   }
+
+  const vector<LookupParameters*>& lookup_params = m.lookup_parameters_list();
+  for (auto pp : lookup_params) {
+    cerr << "\nLOOKUP PARAMETERS " << pp << endl;
+    LookupParameters& p = *pp;
+    size_t ts = p.dim.size();
+    for (unsigned j : p.non_zero_grads) {
+      cerr << "OBJECT=" << j << endl;
+      Tensor& v = p.values[j];
+      Tensor& ag = p.grads[j];
+      for (size_t i = 0; i < ts; ++i) {
+        float old = v.v[i];
+        v.v[i] = old - alpha;
+        float E_left = as_scalar(g.forward());
+
+        v.v[i] = old + alpha;
+        float E_right = as_scalar(g.forward());
+        float g = (E_right - E_left) / (2 * alpha);
+        float f = fabs(g - ag.v[i]);
+        float m = max(fabs(g), fabs(ag.v[i]));
+        if (f > 0.1) {
+          if (m > 0.f) f /= m;
+          if (f > 0.1) { flag = true; cerr << "*** "; }
+        }
+        cerr << ag.v[i] << ' ' << g << endl;
+      }
+    }
+  }
+
   if (flag) {
     cerr << "\n*** GRADIENT CHECK FAILED ***\n";
   } else {
     cerr << "\nGRADIENT CHECK PASSED\n";
   }
-//  const vector<LookupParameters*>& lookup_params = m.lookup_parameters_list();
 }
 
 }
