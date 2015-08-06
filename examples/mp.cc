1102d588f049e281e40d9f2f96278e1481d41854
armatthews <armatthe@cmu.edu>
2015-08-06 14:16:03 -0400

    Factored parent and child process code into methods
    Former-commit-id: 5ebdb9b64ee2ccefb01ddefd95eefcb5a7c88c1a

--- examples/mp.cc
+++ examples/mp.cc
@@ -20,7 +20,9 @@ using namespace std;
 using namespace cnn;
 using namespace cnn::expr;
 
-const unsigned num_children = 10;
+typedef pair<cnn::real, cnn::real> Datum;
+
+const unsigned num_children = 2;
 struct Workload {
   pid_t pid;
   unsigned start;
@@ -43,8 +45,8 @@ void BuildComputationGraph(ComputationGraph& cg, ModelParameters& model_paramete
   Expression loss = squared_distance(y, y_star);
 }
 
-vector<pair<cnn::real, cnn::real>> ReadData(string filename) {
-  vector<pair<cnn::real, cnn::real>> data;
+vector<Datum> ReadData(string filename) {
+  vector<Datum> data;
   ifstream fs(filename);
   string line;
   while (getline(fs, line)) {
@@ -58,6 +60,86 @@ vector<pair<cnn::real, cnn::real>> ReadData(string filename) {
   return data;
 }
 
+unsigned SpawnChildren(vector<Workload>& workloads) {
+  assert (workloads.size() == num_children);
+  pid_t pid;
+  unsigned cid;
+  for (cid = 0; cid < num_children; ++cid) {
+    pid = fork();
+    if (pid == -1) {
+      cerr << "Fork failed. Exiting ...";
+      return 1;
+    }
+    else if (pid == 0) {
+      // children shouldn't continue looping
+      break;
+    }
+    workloads[cid].pid = pid;
+  }
+  return cid;
+}
+
+int RunChild(unsigned cid, ComputationGraph& cg, Trainer* trainer, vector<Workload>& workloads,
+    const vector<Datum>& data, cnn::real& x_value, cnn::real& y_value, ModelParameters& model_params) {
+  assert (cid >= 0 && cid < num_children);
+  unsigned start = workloads[cid].start;
+  unsigned end = workloads[cid].end; 
+  assert (start < end);
+  assert (end <= data.size());
+
+  cnn::real loss = 0;
+  for (auto it = data.begin() + start; it != data.begin() + end; ++it) {
+    auto p = *it;
+    x_value = get<0>(p);
+    y_value = get<1>(p);
+    loss += as_scalar(cg.forward());
+    cg.backward();
+    trainer->update(1.0);
+  }
+  loss /= (end - start);
+
+  cnn::real m_end = as_scalar(model_params.m->values);
+  cnn::real b_end = as_scalar(model_params.b->values);
+
+  write(workloads[cid].pipe[1], (char*)&m_end, sizeof(cnn::real));
+  write(workloads[cid].pipe[1], (char*)&b_end, sizeof(cnn::real));
+  write(workloads[cid].pipe[1], (char*)&loss, sizeof(cnn::real));
+  return 0;
+}
+
+void RunParent(unsigned iter, vector<Workload>& workloads, ModelParameters& model_params, Trainer* trainer) {
+  vector<cnn::real> m_values;
+  vector<cnn::real> b_values;
+  vector<cnn::real> loss_values;
+  for(unsigned cid = 0; cid < num_children; ++cid) {
+    cnn::real m, b, loss;
+    read(workloads[cid].pipe[0], (char*)&m, sizeof(cnn::real));
+    read(workloads[cid].pipe[0], (char*)&b, sizeof(cnn::real));
+    read(workloads[cid].pipe[0], (char*)&loss, sizeof(cnn::real));
+    m_values.push_back(m);
+    b_values.push_back(b);
+    loss_values.push_back(loss);
+    wait(NULL); 
+  }
+
+  cnn::real m = 0.0;
+  cnn::real b = 0.0;
+  cnn::real loss = 0.0;
+  for (unsigned i = 0; i < m_values.size(); ++i) {
+    m += m_values[i];
+    b += b_values[i];
+    loss += loss_values[i];
+  }
+  m /= m_values.size();
+  b /= b_values.size();
+
+  // Update parameters to use the new m and b values
+  TensorTools::SetElements(model_params.m->values, {m});
+  TensorTools::SetElements(model_params.b->values, {b});
+  trainer->update_epoch(); 
+  cerr << iter << "\t" << "loss = " << loss << "\tm = " << m << "\tb = " << b << endl;
+}
+
 int main(int argc, char** argv) {
   cnn::Initialize(argc, argv);
 
@@ -66,7 +148,7 @@ int main(int argc, char** argv) {
     cerr << "Where data.txt contains tab-delimited pairs of floats." << endl;
     return 1;
   }
-  vector<pair<cnn::real, cnn::real>> data = ReadData(argv[1]);
+  vector<Datum> data = ReadData(argv[1]);
   vector<Workload> workloads(num_children);
 
   Model model;
@@ -79,88 +161,24 @@ int main(int argc, char** argv) {
   ModelParameters model_params = {m_param, b_param};
   BuildComputationGraph(cg, model_params, &x_value, &y_value);
 
+  for (unsigned cid = 0; cid < num_children; cid++) {
+    unsigned start = (unsigned)(1.0 * cid / num_children * data.size() + 0.5);
+    unsigned end = (unsigned)(1.0 * (cid + 1) / num_children * data.size() + 0.5);
+    workloads[cid].start = start;
+    workloads[cid].end = end;
+    pipe(workloads[cid].pipe);
+  }
 
   // train the parameters
   for (unsigned iter = 0; true; ++iter) {
     random_shuffle(data.begin(), data.end());
-    pid_t pid;
-    unsigned cid = 0;
-    for (cid = 0; cid < num_children; cid++) {
-      unsigned start = (unsigned)(1.0 * cid / num_children * data.size() + 0.5);
-      unsigned end = (unsigned)(1.0 * (cid + 1) / num_children * data.size() + 0.5);
-      workloads[cid].start = start;
-      workloads[cid].end = end;
-      pipe(workloads[cid].pipe);
-      pid = fork();
-      if (pid == -1) {
-        cerr << "Fork failed. Exiting ...";
-        return 1;
-      }
-      else if (pid == 0) {
-        // children shouldn't continue looping
-        break;
-      }
-      workloads[cid].pid = pid;
-    }
+    unsigned cid = SpawnChildren(workloads);
 
-    if (pid == 0) {
-      assert (cid >= 0 && cid < num_children);
-      unsigned start = workloads[cid].start;
-      unsigned end = workloads[cid].end; 
-      assert (start < end);
-      assert (end <= data.size());
-
-      // Actually do the training thing
-      cnn::real loss = 0;
-      for (auto it = data.begin() + start; it != data.begin() + end; ++it) {
-        auto p = *it;
-        x_value = get<0>(p);
-        y_value = get<1>(p);
-        loss += as_scalar(cg.forward());
-        cg.backward();
-        sgd.update(1.0);
-      }
-      loss /= data.size();
-
-      cnn::real m_end = as_scalar(model_params.m->values);
-      cnn::real b_end = as_scalar(model_params.b->values);
-
-      write(workloads[cid].pipe[1], (char*)&m_end, sizeof(cnn::real));
-      write(workloads[cid].pipe[1], (char*)&b_end, sizeof(cnn::real));
-      write(workloads[cid].pipe[1], (char*)&loss, sizeof(cnn::real));
-      return 0;
+    if (cid < num_children) {
+      return RunChild(cid, cg, &sgd, workloads, data, x_value, y_value, model_params);
     }
     else {
-      vector<cnn::real> m_values;
-      vector<cnn::real> b_values;
-      vector<cnn::real> loss_values;
-      for(unsigned cid = 0; cid < num_children; ++cid) {
-        cnn::real m, b, loss;
-        read(workloads[cid].pipe[0], (char*)&m, sizeof(cnn::real));
-        read(workloads[cid].pipe[0], (char*)&b, sizeof(cnn::real));
-        read(workloads[cid].pipe[0], (char*)&loss, sizeof(cnn::real));
-        m_values.push_back(m);
-        b_values.push_back(b);
-        loss_values.push_back(loss);
-        wait(NULL); 
-      }
-
-      cnn::real m = 0.0;
-      cnn::real b = 0.0;
-      cnn::real loss = 0.0;
-      for (unsigned i = 0; i < m_values.size(); ++i) {
-        m += m_values[i];
-        b += b_values[i];
-        loss += loss_values[i];
-      }
-      m /= m_values.size();
-      b /= b_values.size();
-
-      // Update parameters to use the new m and b values
-      TensorTools::SetElements(m_param->values, {m});
-      TensorTools::SetElements(b_param->values, {b});
-      sgd.update_epoch(); 
-      cerr << iter << "\t" << "loss = " << loss << "\tm = " << m << "\tb = " << b << endl;
+      RunParent(iter, workloads, model_params, &sgd);
     }
   }
 }
