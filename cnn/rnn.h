0600c5dcc3d7e87b022b2b22a32c28366d4167a1
dhg <dhgarrette@gmail.com>
2015-10-21 23:10:02 -0700

    fix override warnings from new version of clang

--- cnn/rnn.h
+++ cnn/rnn.h
@@ -108,12 +108,12 @@ struct SimpleRNNBuilder : public RNNBuilder {
  public:
   Expression add_auxiliary_input(const Expression& x, const Expression &aux);
 
-  Expression back() const override { return (cur == -1? h0.back() : h[cur].back()); }
+  Expression back() const override { return (cur == -1 ? h0.back() : h[cur].back()); }
   std::vector<Expression> final_h() const override { return (h.size() == 0 ? h0 : h.back()); }
   std::vector<Expression> final_s() const override { return final_h(); }
 
-  std::vector<Expression> get_h(RNNPointer i) const { return (i == -1 ? h0 : h[i]); }
-  std::vector<Expression> get_s(RNNPointer i) const { return get_h(i); }
+  std::vector<Expression> get_h(RNNPointer i) const override { return (i == -1 ? h0 : h[i]); }
+  std::vector<Expression> get_s(RNNPointer i) const override { return get_h(i); }
   void copy(const RNNBuilder & params) override;
 
   unsigned num_h0_components() const override { return layers; }
