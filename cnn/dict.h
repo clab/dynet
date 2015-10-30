406d6864ef83179088e1a5715d5eb5cd588826a9
Graham Neubig <neubig@gmail.com>
2015-10-31 04:49:26 +0900

    Fixed compile error on older versions of gcc
    Former-commit-id: d7fa7d7150bfd213140f8c9cd09d5d2c000732ed

--- cnn/dict.h
+++ cnn/dict.h
@@ -9,18 +9,18 @@
 #include <stdexcept>
 
 #include <boost/version.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/string.hpp>
 #if BOOST_VERSION >= 105600
 #include <boost/serialization/split_member.hpp>
-#include <boost/serialization/vector.hpp>
 #include <boost/serialization/unordered_map.hpp>
-#include <boost/serialization/string.hpp>
 #endif
 
 namespace cnn {
 
-  class Dict {
-   typedef std::unordered_map<std::string, int> Map;
- public:
+class Dict {
+typedef std::unordered_map<std::string, int> Map;
+public:
   Dict() : frozen(false), map_unk(false), unk_id(-1) {
   }
 
@@ -37,42 +37,42 @@ namespace cnn {
     auto i = d_.find(word);
     if (i == d_.end()) {
       if (frozen) {
-       if (map_unk) {
-         return unk_id;
-       }
-       else {
-        std::cerr << map_unk << std::endl;
-         std::cerr << "Unknown word encountered: " << word << std::endl;
-         throw std::runtime_error("Unknown word encountered in frozen dictionary: " + word);
-       }
-     }
-     words_.push_back(word);
-     return d_[word] = words_.size() - 1;
-   } else {
-    return i->second;
+        if (map_unk) {
+          return unk_id;
+        }
+        else {
+         std::cerr << map_unk << std::endl;
+          std::cerr << "Unknown word encountered: " << word << std::endl;
+          throw std::runtime_error("Unknown word encountered in frozen dictionary: " + word);
+        }
+      }
+      words_.push_back(word);
+      return d_[word] = words_.size() - 1;
+    } else {
+      return i->second;
+    }
   }
-}
-
-inline const std::string& Convert(const int& id) const {
-  assert(id < (int)words_.size());
-  return words_[id];
-}
-
-void SetUnk(const std::string& word) {
-  if (!frozen)
-    throw std::runtime_error("Please call SetUnk() only after dictionary is frozen");
-  if (map_unk)
-    throw std::runtime_error("Set UNK more than one time");
-
-  // temporarily unfrozen the dictionary to allow the add of the UNK
-  frozen = false;
-  unk_id = Convert(word);
-  frozen = true;
-
-  map_unk = true;
-}
-
-void clear() { words_.clear(); d_.clear(); }
+  
+  inline const std::string& Convert(const int& id) const {
+    assert(id < (int)words_.size());
+    return words_[id];
+  }
+  
+  void SetUnk(const std::string& word) {
+    if (!frozen)
+      throw std::runtime_error("Please call SetUnk() only after dictionary is frozen");
+    if (map_unk)
+      throw std::runtime_error("Set UNK more than one time");
+  
+    // temporarily unfrozen the dictionary to allow the add of the UNK
+    frozen = false;
+    unk_id = Convert(word);
+    frozen = true;
+  
+    map_unk = true;
+  }
+  
+  void clear() { words_.clear(); d_.clear(); }
 
 private:
   bool frozen;
@@ -81,15 +81,19 @@ private:
   std::vector<std::string> words_;
   Map d_;
 
-#if BOOST_VERSION >= 105600
   friend class boost::serialization::access;
+#if BOOST_VERSION >= 105600
   template<class Archive> void serialize(Archive& ar, const unsigned int) {
-  ar & frozen;
-  ar & map_unk;
-  ar & unk_id;
-  ar & words_;
-  ar & d_;
-}
+    ar & frozen;
+    ar & map_unk;
+    ar & unk_id;
+    ar & words_;
+    ar & d_;
+  }
+#else
+  template<class Archive> void serialize(Archive& ar, const unsigned int) {
+    throw std::invalid_argument("Serializing dictionaries is only supported on versions of boost 1.56 or higher");
+  }
 #endif
 };
 
