e56770d4257886c085cd91e08b4cb4744dbd82bb
Graham Neubig <neubig@gmail.com>
2015-11-05 13:43:49 -0500

    Bug fix to the batch example
    Former-commit-id: 4d8b53deb363b0ad1582cb980832923fc9c85eb2

--- cnn/param-nodes.h
+++ cnn/param-nodes.h
@@ -31,6 +31,7 @@ struct InputNode : public Node {
   explicit InputNode(const Dim& d, const std::vector<float>* pd) : dim(d), pdata(pd) {}
   std::string as_string(const std::vector<std::string>& arg_names) const override;
   Dim dim_forward(const std::vector<Dim>& xs) const override;
+  virtual bool supports_multibatch() const override { return true; }  
   void forward_impl(const std::vector<const Tensor*>& xs, Tensor& fx) const override;
   void backward_impl(const std::vector<const Tensor*>& xs,
                   const Tensor& fx,
