88700590592c6206d3ddeb5b3e14e0dbccc678a4
Chris Dyer <cdyer@moto.clab.cs.cmu.edu>
2015-12-08 19:36:10 -0500

    clean up
    Former-commit-id: 1e2169bc293f2952e638115e835cb152c23524e5

--- examples/rnnlm-cfsm.cc
+++ examples/rnnlm-cfsm.cc
@@ -7,7 +7,7 @@
 #include "cnn/lstm.h"
 #include "cnn/dict.h"
 #include "cnn/expr.h"
-#include "cnn/hsm-builder.h"
+#include "cnn/cfsm-builder.h"
 
 #include <iostream>
 #include <fstream>
@@ -32,18 +32,18 @@ template <class Builder>
 struct RNNLanguageModel {
   LookupParameters* p_c;
   Builder builder;
-  HierarchicalSoftmaxBuilder& hsm;
-  explicit RNNLanguageModel(Model& model, HierarchicalSoftmaxBuilder& h) :
+  ClassFactoredSoftmaxBuilder& cfsm;
+  explicit RNNLanguageModel(Model& model, ClassFactoredSoftmaxBuilder& h) :
       p_c(model.add_lookup_parameters(VOCAB_SIZE, {INPUT_DIM})),
       builder(LAYERS, INPUT_DIM, HIDDEN_DIM, &model),
-      hsm(h) {}
+      cfsm(h) {}
 
   // return Expression of total loss
   Expression BuildLMGraph(const vector<int>& sent, ComputationGraph& cg) {
     const unsigned slen = sent.size() - 1;
     builder.new_graph(cg);  // reset RNN builder for new graph
     builder.start_new_sequence();
-    hsm.new_graph(cg);
+    cfsm.new_graph(cg);
     //Expression i_R = parameter(cg, p_R); // hidden -> word rep parameter
     //Expression i_bias = parameter(cg, p_bias);  // word bias
     vector<Expression> errs;
@@ -51,7 +51,7 @@ struct RNNLanguageModel {
       Expression i_x_t = lookup(cg, p_c, sent[t]);
       // y_t = RNN(x_t)
       Expression i_y_t = builder.add_input(i_x_t);
-      Expression i_err = hsm.neg_log_softmax(i_y_t, sent[t+1]);
+      Expression i_err = cfsm.neg_log_softmax(i_y_t, sent[t+1]);
       errs.push_back(i_err);
     }
     Expression i_nerr = sum(errs);
@@ -62,7 +62,7 @@ struct RNNLanguageModel {
   void RandomSample(int max_len = 150) {
     cerr << endl;
     ComputationGraph cg;
-    hsm.new_graph(cg);
+    cfsm.new_graph(cg);
     builder.new_graph(cg);  // reset RNN builder for new graph
     builder.start_new_sequence();
     vector<Expression> errs;
@@ -73,7 +73,7 @@ struct RNNLanguageModel {
       Expression i_x_t = lookup(cg, p_c, cur);
       // y_t = RNN(x_t)
       Expression i_y_t = builder.add_input(i_x_t);
-      cur = hsm.sample(i_y_t);
+      cur = cfsm.sample(i_y_t);
       cerr << (len == 1 ? "" : " ") << d.Convert(cur);
     }
     cerr << endl;
@@ -89,7 +89,7 @@ int main(int argc, char** argv) {
   kSOS = d.Convert("<s>");
   kEOS = d.Convert("</s>");
   Model model;
-  HierarchicalSoftmaxBuilder hsm(HIDDEN_DIM, argv[3], &d, &model);
+  ClassFactoredSoftmaxBuilder cfsm(HIDDEN_DIM, argv[3], &d, &model);
   vector<vector<int>> training, dev;
   string line;
   int tlc = 0;
@@ -130,7 +130,7 @@ int main(int argc, char** argv) {
     cerr << dlc << " lines, " << dtoks << " tokens\n";
   }
   ostringstream os;
-  os << "hsmlm"
+  os << "cfsmlm"
      << '_' << LAYERS
      << '_' << INPUT_DIM
      << '_' << HIDDEN_DIM
@@ -146,9 +146,11 @@ int main(int argc, char** argv) {
   //else
   sgd = new SimpleSGDTrainer(&model);
 
-  RNNLanguageModel<LSTMBuilder> lm(model, hsm);
-  //RNNLanguageModel<SimpleRNNBuilder> lm(model, hsm);
+  RNNLanguageModel<LSTMBuilder> lm(model, cfsm);
+  //RNNLanguageModel<SimpleRNNBuilder> lm(model, cfsm);
+  bool has_loaded_model = false;
   if (argc == 5) {
+    has_loaded_model = true;
     string fname = argv[4];
     ifstream in(fname);
     boost::archive::text_iarchive ia(in);
@@ -167,6 +169,7 @@ int main(int argc, char** argv) {
     Timer iteration("completed in");
     double loss = 0;
     unsigned chars = 0;
+#if 1
     for (unsigned i = 0; i < report_every_i; ++i) {
       if (si == training.size()) {
         si = 0;
@@ -178,7 +181,7 @@ int main(int argc, char** argv) {
       // build graph for this instance
       ComputationGraph cg;
       auto& sent = training[order[si]];
-      chars += sent.size() - 1;
+      chars += sent.size() - 2;
       ++si;
       lm.BuildLMGraph(sent, cg);
       loss += as_scalar(cg.forward());
@@ -193,22 +196,25 @@ int main(int argc, char** argv) {
     // show score on dev data?
     report++;
     if (report % dev_every_i_reports == 0) {
+#endif
       double dloss = 0;
       int dchars = 0;
       for (auto& sent : dev) {
         ComputationGraph cg;
         lm.BuildLMGraph(sent, cg);
         dloss += as_scalar(cg.forward());
-        dchars += sent.size() - 1;
+        dchars += sent.size() - 2;
       }
+      cerr << "\n***DEV [epoch=" << (lines / (double)training.size()) << "] E = " << (dloss / dchars) << " ppl=" << exp(dloss / dchars) << ' ';
+#if 1
       if (dloss < best) {
         best = dloss;
         ofstream out(fname);
         boost::archive::text_oarchive oa(out);
         oa << model;
       }
-      cerr << "\n***DEV [epoch=" << (lines / (double)training.size()) << "] E = " << (dloss / dchars) << " ppl=" << exp(dloss / dchars) << ' ';
     }
+#endif
   }
   delete sgd;
 }
