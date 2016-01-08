601c8dc0f32755844ffd056c4fcd13cde4dc7fd1
Graham Neubig <neubig@gmail.com>
2016-01-08 15:23:10 -0500

    Batching support for transpose and dropout
    Former-commit-id: 3dcf789246bc7ce99669d19209580a98e64997e9

--- cnn/nodes.h
+++ cnn/nodes.h
@@ -94,6 +94,7 @@ struct Transpose : public Node {
   explicit Transpose(const std::initializer_list<VariableIndex>& a) : Node(a) {}
   std::string as_string(const std::vector<std::string>& arg_names) const override;
   Dim dim_forward(const std::vector<Dim>& xs) const override;
+  virtual bool supports_multibatch() const override { return true; }
   void forward_impl(const std::vector<const Tensor*>& xs, Tensor& fx) const override;
   void backward_impl(const std::vector<const Tensor*>& xs,
                   const Tensor& fx,
@@ -186,6 +187,7 @@ struct Dropout : public Node {
   std::string as_string(const std::vector<std::string>& arg_names) const override;
   Dim dim_forward(const std::vector<Dim>& xs) const override;
   size_t aux_storage_size() const override;
+  virtual bool supports_multibatch() const override { return true; }
   void forward_impl(const std::vector<const Tensor*>& xs, Tensor& fx) const override;
   void backward_impl(const std::vector<const Tensor*>& xs,
                 const Tensor& fx,
