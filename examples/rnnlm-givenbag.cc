bd4198880ea15ba01cf34fddf95d00b604548e71
Chris Dyer <redpony@gmail.com>
2016-01-18 15:53:11 -0500

    example of RNN that conditions on the set of words in the sentence
    Former-commit-id: 06124a6bdfcc677aea9f4b0c6783a768018e6ecf

---
+++ examples/rnnlm-givenbag.cc
@@ -0,0 +1,253 @@
+#include "cnn/nodes.h"
+#include "cnn/cnn.h"
+#include "cnn/training.h"
+#include "cnn/timing.h"
+#include "cnn/rnn.h"
+#include "cnn/gru.h"
+#include "cnn/lstm.h"
+#include "cnn/dict.h"
+# include "cnn/expr.h"
+#include "cnn/grad-check.h"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+
+using namespace std;
+using namespace cnn;
+
+unsigned LAYERS = 2;
+unsigned INPUT_DIM = 8;  //256
+unsigned HIDDEN_DIM = 24;  // 1024
+unsigned VOCAB_SIZE = 0;
+
+cnn::Dict d;
+int kSOS;
+int kEOS;
+
+template <class Builder>
+struct RNNLanguageModel {
+  LookupParameters* p_c;
+  Parameters* p_R;
+  Parameters* p_bias;
+  Builder builder;
+  explicit RNNLanguageModel(Model& model) : builder(LAYERS, INPUT_DIM, HIDDEN_DIM, &model) {
+    p_c = model.add_lookup_parameters(VOCAB_SIZE, {INPUT_DIM}); 
+    p_R = model.add_parameters({VOCAB_SIZE, HIDDEN_DIM});
+    p_bias = model.add_parameters({VOCAB_SIZE});
+  }
+
+  // return Expression of total loss
+  Expression BuildLMGraph(const vector<int>& sent, ComputationGraph& cg, const vector<unsigned>* prows) {
+    const unsigned slen = sent.size() - 1;
+    builder.new_graph(cg);  // reset RNN builder for new graph
+    builder.start_new_sequence();
+    Expression i_R = parameter(cg, p_R); // hidden -> word rep parameter
+    i_R = select_rows(i_R, prows);  // FILTER rows for bag (prows)
+    Expression i_bias = parameter(cg, p_bias);  // word bias
+    i_bias = select_rows(i_bias, prows);  // FILTER rows for bag
+    vector<Expression> errs;
+    for (unsigned t = 0; t < slen; ++t) {
+      // lookup uses prows since sent[t] is the mapped row
+      Expression i_x_t = lookup(cg, p_c, (*prows)[sent[t]]);
+      // y_t = RNN(x_t)
+      Expression i_y_t = builder.add_input(i_x_t);
+      Expression i_r_t =  i_bias + i_R * i_y_t;
+      
+      Expression lsm = log_softmax(i_r_t);
+      Expression err = -pick(lsm, sent[t+1]);
+      errs.push_back(err);
+    }
+    Expression i_nerr = sum(errs);
+    return i_nerr;
+  }
+
+  // return Expression for total loss
+  void RandomSample(int max_len = 150) {
+    cerr << endl;
+    ComputationGraph cg;
+    builder.new_graph(cg);  // reset RNN builder for new graph
+    builder.start_new_sequence();
+    
+    Expression i_R = parameter(cg, p_R);
+    Expression i_bias = parameter(cg, p_bias);
+    vector<Expression> errs;
+    int len = 0;
+    int cur = kSOS;
+    while(len < max_len && cur != kEOS) {
+      ++len;
+      Expression i_x_t = lookup(cg, p_c, cur);
+      // y_t = RNN(x_t)
+      Expression i_y_t = builder.add_input(i_x_t);
+      Expression i_r_t = i_bias + i_R * i_y_t;
+      
+      Expression ydist = softmax(i_r_t);
+      
+      unsigned w = 0;
+      while (w == 0 || (int)w == kSOS) {
+        auto dist = as_vector(cg.incremental_forward());
+        double p = rand01();
+        for (; w < dist.size(); ++w) {
+          p -= dist[w];
+          if (p < 0.0) { break; }
+        }
+        if (w == dist.size()) w = kEOS;
+      }
+      cerr << (len == 1 ? "" : " ") << d.Convert(w);
+      cur = w;
+    }
+    cerr << endl;
+  }
+};
+
+int main(int argc, char** argv) {
+  cnn::Initialize(argc, argv);
+  if (argc != 3 && argc != 4) {
+    cerr << "Usage: " << argv[0] << " corpus.txt dev.txt [model.params]\n";
+    return 1;
+  }
+  kSOS = d.Convert("<s>");
+  kEOS = d.Convert("</s>");
+  vector<vector<int>> training, dev;
+  string line;
+  int tlc = 0;
+  int ttoks = 0;
+  cerr << "Reading training data from " << argv[1] << "...\n";
+  {
+    ifstream in(argv[1]);
+    assert(in);
+    while(getline(in, line)) {
+      ++tlc;
+      training.push_back(ReadSentence(line, &d));
+      ttoks += training.back().size();
+      if (training.back().front() != kSOS && training.back().back() != kEOS) {
+        cerr << "Training sentence in " << argv[1] << ":" << tlc << " didn't start or end with <s>, </s>\n";
+        abort();
+      }
+    }
+    cerr << tlc << " lines, " << ttoks << " tokens, " << d.size() << " types\n";
+  }
+  d.Freeze(); // no new word types allowed
+  VOCAB_SIZE = d.size();
+
+  int dlc = 0;
+  int dtoks = 0;
+  cerr << "Reading dev data from " << argv[2] << "...\n";
+  {
+    ifstream in(argv[2]);
+    assert(in);
+    while(getline(in, line)) {
+      ++dlc;
+      dev.push_back(ReadSentence(line, &d));
+      dtoks += dev.back().size();
+      if (dev.back().front() != kSOS && dev.back().back() != kEOS) {
+        cerr << "Dev sentence in " << argv[2] << ":" << tlc << " didn't start or end with <s>, </s>\n";
+        abort();
+      }
+    }
+    cerr << dlc << " lines, " << dtoks << " tokens\n";
+  }
+  ostringstream os;
+  os << "lm"
+     << '_' << LAYERS
+     << '_' << INPUT_DIM
+     << '_' << HIDDEN_DIM
+     << "-pid" << getpid() << ".params";
+  const string fname = os.str();
+  cerr << "Parameters will be written to: " << fname << endl;
+  double best = 9e+99;
+
+  Model model;
+  bool use_momentum = false;
+  Trainer* sgd = nullptr;
+  //if (use_momentum)
+  //else
+  //sgd = new SimpleSGDTrainer(&model);
+  sgd = new MomentumSGDTrainer(&model);
+  RNNLanguageModel<LSTMBuilder> lm(model);
+  //RNNLanguageModel<SimpleRNNBuilder> lm(model);
+  if (argc == 4) {
+    string fname = argv[3];
+    ifstream in(fname);
+    boost::archive::text_iarchive ia(in);
+    ia >> model;
+  }
+
+  unsigned report_every_i = 50;
+  unsigned dev_every_i_reports = 500;
+  unsigned si = training.size();
+  vector<unsigned> order(training.size());
+  for (unsigned i = 0; i < order.size(); ++i) order[i] = i;
+  bool first = true;
+  int report = 0;
+  unsigned lines = 0;
+  vector<unsigned> rows;
+  vector<unsigned> w2sl(VOCAB_SIZE, 0);
+  vector<int> rmsent;
+  while(1) {
+    Timer iteration("completed in");
+    double loss = 0;
+    unsigned chars = 0;
+    for (unsigned i = 0; i < report_every_i; ++i) {
+      if (si == training.size()) {
+        si = 0;
+        if (first) { first = false; } else { sgd->update_epoch(); }
+        cerr << "**SHUFFLE\n";
+        shuffle(order.begin(), order.end(), *rndeng);
+      }
+
+      // build graph for this instance
+      ComputationGraph cg;
+      auto& sent = training[order[si]];
+      int oi = 0;
+      int idx = 0;
+      rmsent.resize(sent.size());
+      rows.resize(sent.size());
+      for (auto w : sent) {
+        auto& rmw = w2sl[w];
+        if (rmw == 0) { rows[idx] = w; rmw = idx; idx++; }
+        rmsent[oi] = rmw;
+        oi++;
+      }
+      rows.resize(idx); // in case of duplicates
+      chars += sent.size() - 1;
+      ++si;
+      lm.BuildLMGraph(rmsent, cg, &rows);
+      loss += as_scalar(cg.forward());
+      cg.backward();
+      sgd->update();
+      for (auto w : sent) { w2sl[w] = 0; }
+      ++lines;
+    }
+    sgd->status();
+    cerr << " E = " << (loss / chars) << " ppl=" << exp(loss / chars) << ' ';
+#if 0
+    lm.RandomSample();
+
+    // show score on dev data?
+    report++;
+    if (report % dev_every_i_reports == 0) {
+      double dloss = 0;
+      int dchars = 0;
+      for (auto& sent : dev) {
+        ComputationGraph cg;
+        lm.BuildLMGraph(sent, cg);
+        dloss += as_scalar(cg.forward());
+        dchars += sent.size() - 1;
+      }
+      if (dloss < best) {
+        best = dloss;
+        ofstream out(fname);
+        boost::archive::text_oarchive oa(out);
+        oa << model;
+      }
+      cerr << "\n***DEV [epoch=" << (lines / (double)training.size()) << "] E = " << (dloss / dchars) << " ppl=" << exp(dloss / dchars) << ' ';
+    }
+#endif
+  }
+  delete sgd;
+}
+
