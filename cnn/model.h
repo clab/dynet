d05571bd551961cfe5465cac78eedc5a72390711
austinma <austinma@cs.cmu.edu>
2016-02-02 04:56:30 -0500

    Examples showing new serialization
    Former-commit-id: 1849db6b825287d19754eab4b6320a37414b942a

--- cnn/model.h
+++ cnn/model.h
@@ -5,9 +5,12 @@
 #include <unordered_set>
 #include <string>
 
-
-#include <boost/serialization/split_member.hpp>
+#include <boost/serialization/export.hpp>
 #include <boost/serialization/vector.hpp>
+#include <boost/serialization/serialization.hpp>
+#include <boost/serialization/access.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
 
 #include "cnn/tensor.h"
 
@@ -26,6 +29,9 @@ struct ParametersBase {
   virtual void g_squared_l2norm(float* sqnorm) const = 0;
   virtual size_t size() const = 0;
   virtual ~ParametersBase();
+  friend class boost::serialization::access;
+  template<class Archive> 
+  void serialize(Archive& ar, const unsigned int) {}
 };
 
 // represents parameters (e.g., a weight matrix) that will be optimized
@@ -48,9 +54,12 @@ struct Parameters : public ParametersBase {
   explicit Parameters(const Dim& d, float minmax); // initialize with ~U(-minmax,+minmax)
                                  // or Glorot initialization if minmax = 0
   friend class boost::serialization::access;
-  template<class Archive> void serialize(Archive& ar, const unsigned int) {
+  template<class Archive>
+  void serialize(Archive& ar, const unsigned int) {
+    boost::serialization::base_object<ParametersBase>(*this);
     ar & dim;
     ar & values;
+    ar & g;
   }
 };
 
@@ -77,23 +86,12 @@ struct LookupParameters : public ParametersBase {
   LookupParameters(unsigned n, const Dim& d);
   friend class boost::serialization::access;
   template<class Archive>
-  void save(Archive& ar, const unsigned int) const {
+  void serialize(Archive& ar, const unsigned int) {
+    boost::serialization::base_object<ParametersBase>(*this);
     ar & dim;
-    int nv = values.size();
-    ar & nv;
-    for (unsigned i = 0; i < values.size(); ++i)
-      ar & values[i];
+    ar & values;
+    ar & grads;
   }
-  template<class Archive>
-  void load(Archive& ar, const unsigned int) {
-    ar & dim;
-    int nv;
-    ar & nv;
-    assert(nv == (int)values.size());
-    for (unsigned i = 0; i < values.size(); ++i)
-      ar & values[i];
-  }
-  BOOST_SERIALIZATION_SPLIT_MEMBER()
 };
 
 class Model;
@@ -108,6 +106,14 @@ struct ParameterIndex {
 
   const Model* mp;
   unsigned long index;
+
+private:
+  friend class boost::serialization::access;
+  template<class Archive>
+  void serialize(Archive& ar, const unsigned int) {
+    ar & mp;
+    ar & index;
+  }
 };
 
 struct LookupParameterIndex {
@@ -122,6 +128,14 @@ struct LookupParameterIndex {
 
   const Model* mp;
   unsigned long index;
+
+private:
+  friend class boost::serialization::access;
+  template<class Archive>
+  void serialize(Archive& ar, const unsigned int) {
+    ar & mp;
+    ar & index;
+  }
 };
 
 // this is a collection of parameters
@@ -147,32 +161,11 @@ class Model {
  private:
   friend class boost::serialization::access;
   template<class Archive>
-  void save(Archive& ar, const unsigned int) const {
-    int np = params.size();
-    int nlp = lookup_params.size();
-    ar & np;
-    ar & nlp;
-    for (unsigned i = 0; i < params.size(); ++i)
-      ar & *params[i];
-    for (unsigned i = 0; i < lookup_params.size(); ++i)
-      ar & *lookup_params[i];
+  void serialize(Archive& ar, const unsigned int) {
+    ar & all_params;
+    ar & params;
+    ar & lookup_params;
   }
-  template<class Archive>
-  void load(Archive& ar, const unsigned int) {
-    int np, nlp;
-    ar & np;
-    ar & nlp;
-    assert(np == (int)params.size());
-    assert(nlp == (int)lookup_params.size());
-    for (unsigned i = 0; i < params.size(); ++i)
-      ar & *params[i];
-    for (unsigned i = 0; i < lookup_params.size(); ++i)
-      ar & *lookup_params[i];
-    all_params.clear();
-    for (auto p : params) all_params.push_back(p);
-    for (auto p : lookup_params) all_params.push_back(p);
-  }
-  BOOST_SERIALIZATION_SPLIT_MEMBER()
 
   std::vector<ParametersBase*> all_params;
   std::vector<Parameters*> params;
@@ -184,5 +177,7 @@ void save_cnn_model(std::string filename, Model* model);
 void load_cnn_model(std::string filename, Model* model);
 
 } // namespace cnn
+BOOST_CLASS_EXPORT_KEY(cnn::Parameters)
+BOOST_CLASS_EXPORT_KEY(cnn::LookupParameters)
 
 #endif
