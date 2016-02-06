4f0475e3f92fc045c298da0dcb2fd8e9f952cd41
Chris Dyer <redpony@gmail.com>
2016-02-06 02:17:56 -0500

    couldn't resist
    Former-commit-id: 817f5bbac26ca366acf7f0e910db75adf9b87392

--- cnn/expr.cc
+++ cnn/expr.cc
@@ -56,6 +56,7 @@ Expression hinge(const Expression& x, unsigned index, float m) { return Expressi
 Expression hinge(const Expression& x, const unsigned* pindex, float m) { return Expression(x.pg, x.pg->add_function<Hinge>({x.i}, pindex, m)); }
 Expression log_softmax(const Expression& x) { return Expression(x.pg, x.pg->add_function<LogSoftmax>({x.i})); }
 Expression log_softmax(const Expression& x, const vector<unsigned>& d) { return Expression(x.pg, x.pg->add_function<RestrictedLogSoftmax>({x.i}, d)); }
+Expression sparsemax(const Expression& x) { return Expression(x.pg, x.pg->add_function<Sparsemax>({x.i})); }
 Expression softmax(const Expression& x) { return Expression(x.pg, x.pg->add_function<Softmax>({x.i})); }
 Expression softsign(const Expression& x) { return Expression(x.pg, x.pg->add_function<SoftSign>({x.i})); }
 Expression pow(const Expression& x, const Expression& y) { return Expression(x.pg, x.pg->add_function<Pow>({x.i, y.i})); }
