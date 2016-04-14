cd116f0797185316189a0da9430fa6b678f8b0d0
Yoav Goldberg <yoav.goldberg@gmail.com>
2016-04-14 15:28:55 +0300

    GPU in pycnn

--- pycnn/setup_gpu.py
+++ pycnn/setup_gpu.py
@@ -10,19 +10,21 @@ if "CFLAGS" in cfg_vars:
        cfg_vars["CFLAGS"] = cfg_vars["CFLAGS"].replace("-Wstrict-prototypes", "")
 
 ext = Extension(
-        "pycnn",                 # name of extension
-        ["pycnn.pyx"],           # filename of our Pyrex/Cython source
+        "gpycnn",                 # name of extension
+        ["gpycnn.pyx"],           # filename of our Pyrex/Cython source
         language="c++",              # this causes Pyrex/Cython to create C++ source
         include_dirs=["../../cnn/", # this is the location of the main cnn directory.
                       "../../eigen/"], # this is the directory where eigen is saved.
-        libraries=['cnn_shared'],             # ditto
+        #libraries=['cnn','cnncuda'], #,'cnncuda_shared'],             # ditto
+        libraries=['gcnn_shared','cnncuda_shared'],             # ditto
         library_dirs=["."],
         #extra_link_args=["-L/home/yogo/Vork/Research/cnn/cnn/build/cnn"],       # if needed
-        extra_compile_args=["-std=c++11"],
-        runtime_library_dirs=["$ORIGIN/./"],
+        extra_compile_args=["-std=c++11","-fPIC"],#,"-lcudart","-lcublas"],
+		extra_link_args=["-L/usr/local/cuda-7.5/lib64","-lcudart","-lcublas"],
+		#extra_objects=["libcnncuda.a"],
         )
 
 setup(ext_modules = [ext],
         cmdclass = {'build_ext': build_ext},
         name="pyCNN",
-	 )
+        )
