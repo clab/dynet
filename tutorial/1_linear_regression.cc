859deb5da06665e71fe7f102b0366570c3030b60
austinma <austinma@cs.cmu.edu>
2016-05-03 00:06:54 -0400

    Started porting theano tutorials to cnn
    Former-commit-id: 28c682059a7d5dd49fa9ec2d0a4b9fd0b5756354

---
+++ tutorial/1_linear_regression.cc
@@ -0,0 +1,44 @@
+#include "cnn/cnn.h"
+#include "cnn/training.h"
+#include "cnn/expr.h"
+
+#include <iostream>
+#include <random>
+
+using namespace std;
+using namespace cnn;
+using namespace cnn::expr;
+
+int main(int argc, char** argv) {
+  cnn::Initialize(argc, argv);
+
+  default_random_engine rng();
+  normal_distribution<float> normal(0.0f, 1.0f);
+
+  vector<float> xs;
+  vector<float> ys;
+  for (unsigned i = 0; i < 100; ++i) {
+    float x = -1 + 2.0 / 100;
+    float y = 2 * x + normal(rng) * 0.33f;
+    xs.push_back(x);
+    ys.push_back(y);
+  }
+
+  Model model;
+  Parameter pW = model.add_parameters({1});
+
+  SimpleSGDTrainer trainer(&model, 0.1);
+
+  ComputationGraph cg;
+  Expression W = parameter(cg, pW);
+
+  for (unsigned i = 0; i < xs.size(); ++i) {
+    Expression pred = W * xs[i];
+    Expression loss = square(pred - ys[i]);
+    cg.forward();
+    cg.backward();
+    trainer.update();
+  }
+
+  cout << as_scalar(W.value()) << endl; // something around 2
+}
