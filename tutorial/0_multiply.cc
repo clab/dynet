28c682059a7d5dd49fa9ec2d0a4b9fd0b5756354
austinma <austinma@cs.cmu.edu>
2016-05-03 00:06:54 -0400

    Started porting theano tutorials to cnn

---
+++ tutorial/0_multiply.cc
@@ -0,0 +1,27 @@
+#include "cnn/cnn.h"
+#include "cnn/training.h"
+#include "cnn/expr.h"
+
+#include <iostream>
+
+using namespace std;
+using namespace cnn;
+using namespace cnn::expr;
+
+int main(int argc, char** argv) {
+  cnn::Initialize(argc, argv);
+
+  ComputationGraph cg;
+  float ia, ib;
+  Expression a = input(cg, &ia);
+  Expression b = input(cg, &ib);
+  Expression y = a * b;
+
+  ia = 1;
+  ib = 2;
+  cout << as_scalar(cg.forward()) << endl; // 2
+
+  ia = 3;
+  ib = 3;
+  cout << as_scalar(cg.forward()) << endl; // 9
+}
