8f6b3124c904929077de943d933863e1c5bb8b02
Graham Neubig <neubig@gmail.com>
2016-06-11 19:36:19 +0900

    Fixed missing initialization in hinge loss
    Former-commit-id: 737deb7f8e083f6cba6e0d3fdde8f91faa0cbf7c

--- cnn/nodes.h
+++ cnn/nodes.h
@@ -270,8 +270,8 @@ struct PairwiseRankLoss : public Node {
 struct Hinge : public Node {
   explicit Hinge(const std::initializer_list<VariableIndex>& a, unsigned e, real m = 1.0) : Node(a), element(e), pelement(&element), margin(m) {}
   explicit Hinge(const std::initializer_list<VariableIndex>& a, const unsigned* pe, real m = 1.0) : Node(a), element(), pelement(pe), margin(m) {}
-  explicit Hinge(const std::initializer_list<VariableIndex>& a, const std::vector<unsigned>& e, real m = 1.0) : Node(a), element(), pelement(), elements(e), pelements(&elements) {}
-  explicit Hinge(const std::initializer_list<VariableIndex>& a, const std::vector<unsigned>* pe, real m = 1.0) : Node(a), element(), pelement(), elements(), pelements(pe) {}
+  explicit Hinge(const std::initializer_list<VariableIndex>& a, const std::vector<unsigned>& e, real m = 1.0) : Node(a), element(), pelement(), elements(e), pelements(&elements), margin(m) {}
+  explicit Hinge(const std::initializer_list<VariableIndex>& a, const std::vector<unsigned>* pe, real m = 1.0) : Node(a), element(), pelement(), elements(), pelements(pe), margin(m) {}
   CNN_NODE_DEFINE_DEV_IMPL()
   size_t aux_storage_size() const override;
   unsigned element;
