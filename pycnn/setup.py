d6c1ccbf8317363216c224d1c5547f93019fa5f8
yoavg <yoav.goldberg@gmail.com>
2016-07-08 17:15:08 +0300

    --pycnn-viz option

--- pycnn/setup.py
+++ pycnn/setup.py
@@ -9,8 +9,15 @@ cfg_vars = distutils.sysconfig.get_config_vars()
 if "CFLAGS" in cfg_vars:
        cfg_vars["CFLAGS"] = cfg_vars["CFLAGS"].replace("-Wstrict-prototypes", "")
 
+COMPILER_ARGS=["-std=c++11"]
+import platform
+
+# in some OSX systems, the following extra flags are needed:
+if platform.system() == 'Darwin':
+    COMPILER_ARGS.extend(["-stdlib=libc++","-mmacosx-version-min=10.7"])
+
 ext = Extension(
-        "pycnn",                 # name of extension
+        "_pycnn",                 # name of extension
         ["pycnn.pyx"],           # filename of our Pyrex/Cython source
         language="c++",              # this causes Pyrex/Cython to create C++ source
         include_dirs=["../../cnn/", # this is the location of the main cnn directory.
@@ -18,11 +25,12 @@ ext = Extension(
         libraries=['cnn_shared'],             # ditto
         library_dirs=["."],
         #extra_link_args=["-L/home/yogo/Vork/Research/cnn/cnn/build/cnn"],       # if needed
-        extra_compile_args=["-std=c++11"],
+        extra_compile_args=COMPILER_ARGS,
         runtime_library_dirs=["$ORIGIN/./"],
         )
 
 setup(ext_modules = [ext],
         cmdclass = {'build_ext': build_ext},
         name="pyCNN",
-	 )
+        py_modules = ['pycnn','pycnn_viz'],
+)
