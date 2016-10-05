05f31b7b4de27d598555e3e6cd35c3f068893947
Yoav Goldberg <yogo@nlp1.local>
2016-10-05 20:00:37 +0300

    gpu support for pycnn.

--- pycnn/pycnn.py
+++ pycnn/pycnn.py
@@ -1,6 +1,10 @@
 import sys
 if '--pycnn-viz' in sys.argv:
     from pycnn_viz import *
+elif '--pycnn-gpu' in sys.argv:
+    def print_graphviz(**kwarge):
+        print "Run with --pycnn-viz to get the visualization behavior."
+    from _gpycnn import *
 else:
     def print_graphviz(**kwarge):
         print "Run with --pycnn-viz to get the visualization behavior."
