172bcec05cf4062c462c420ef249db610a9744b0
Yoav Goldberg <yogo@nlp1.local>
2016-10-08 12:20:48 +0300

    fixed gpu import in pydynet

--- python/dynet.py
+++ python/dynet.py
@@ -1,7 +1,13 @@
 import sys
 if '--dynet-viz' in sys.argv:
+    sys.argv.remove('--dynet-viz')
     from dynet_viz import *
-elif '--dynet-gpu' in sys.argv:
+elif '--dynet-gpu' in sys.argv:  # the python gpu switch.
+    sys.argv.remove('--dynet-gpu')
+    def print_graphviz(**kwarge):
+        print "Run with --dynet-viz to get the visualization behavior."
+    from _gdynet import *
+elif '--dynet-gpus' in sys.argv or '--dynet-gpu-ids' in sys.argv: # but using the c++ gpu switches suffices to trigger gpu.
     def print_graphviz(**kwarge):
         print "Run with --dynet-viz to get the visualization behavior."
     from _gdynet import *
