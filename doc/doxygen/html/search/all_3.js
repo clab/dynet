c1792479f5a10353501348d54032a4d76b5b774c
paul <pmichel31415@gmail.com>
2016-10-14 14:21:23 -0400

    removed generated files

--- doc/doxygen/html/search/all_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['rmsproptrainer',['RmsPropTrainer',['../structdynet_1_1RmsPropTrainer.html',1,'dynet']]]
-];
--- doc/doxygen/html/search/functions_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['input',['input',['../structdynet_1_1expr_1_1Expression.html#a2f4637c96739a26fb29061ca6e53be72',1,'dynet::expr::Expression']]]
-];
