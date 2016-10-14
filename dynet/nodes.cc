d0fb01631e90b24e135957c47c00b478bc2943ff
Graham Neubig <neubig@gmail.com>
2016-10-14 15:26:39 -0400

    Reverted change to hinge that was breaking tests
    Former-commit-id: 180f87f87e5b8df1756077b921022b72cee41712

--- dynet/nodes.cc
+++ dynet/nodes.cc
@@ -782,8 +782,7 @@ void Hinge::backward_dev_impl(const MyDevice & dev,
 #if defined(__CUDACC__) && defined(EIGEN_NO_MALLOC)
       throw std::runtime_error("CUDA memory allocation in hinge");
 #endif
-      auto&& hinge_sum = (eloss.tvec() > 0.f).cast<float>().sum() * d;
-      dEdxi.tvec().chip<0>(*pelement).device(*dev.edevice) -= hinge_sum;
+      dEdxi.tvec().chip<0>(*pelement).device(*dev.edevice) -= (eloss.tvec() > 0.f).cast<float>().sum() * d;
     }
   } else {
     assert(pelements != nullptr); 
@@ -795,10 +794,9 @@ void Hinge::backward_dev_impl(const MyDevice & dev,
         // TODO: The > comparison should not be calculated twice. Keep it in auxiliary memory?
         dEdxi.tb<1>().chip<1>(b).device(*dev.edevice) += (eloss.tb<1>().chip<1>(b) > 0.f).cast<float>() * d_vec[b];
 #if defined(__CUDACC__) && defined(EIGEN_NO_MALLOC)
-    throw std::runtime_error("CUDA memory allocation in hinge");
+        throw std::runtime_error("CUDA memory allocation in hinge");
 #endif
-		auto&& hinge_sum = (eloss.tb<1>().chip<1>(b) > 0.f).cast<float>().sum() * d_vec[b];
-		dEdxi.tb<1>().chip<1>(b).chip<0>((*pelements)[b]).device(*dev.edevice) -= hinge_sum;
+        dEdxi.tb<1>().chip<1>(b).chip<0>((*pelements)[b]).device(*dev.edevice) -= (eloss.tb<1>().chip<1>(b) > 0.f).cast<float>().sum() * d_vec[b];
       }
     }
   }
