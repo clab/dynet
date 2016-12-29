3f027bf70ec80e4d86779cc4457fbc441ab34705
Graham Neubig <neubig@gmail.com>
2016-12-29 13:36:06 +0000

    Efficiency improvement for SimpleRNN
    Former-commit-id: 34f3d16c5ad3d9a16ff92a994af5f2e8f0142cfc

--- dynet/rnn.cc
+++ dynet/rnn.cc
@@ -112,17 +112,15 @@ Expression SimpleRNNBuilder::add_input_impl(int prev, const Expression &in) {
   for (unsigned i = 0; i < layers; ++i) {
     const vector<Expression>& vars = param_vars[i];
 
-    // y <--- f(x)
-    Expression y = affine_transform({vars[2], vars[0], x});
-
     // y <--- g(y_prev)
-    if (prev == -1 && h0.size() > 0)
-      y = affine_transform({y, vars[1], h0[i]});
-    else if (prev >= 0)
-      y = affine_transform({y, vars[1], h[prev][i]});
+    if(prev >= 0) {
+      x = h[t][i] = tanh( affine_transform({vars[2], vars[0], x, vars[1], h[prev][i]}) );
+    } else if(h0.size() > 0) {
+      x = h[t][i] = tanh( affine_transform({vars[2], vars[0], x, vars[1], h0[i]}) );
+    } else {
+      x = h[t][i] = tanh( affine_transform({vars[2], vars[0], x}) );
+    }
 
-    // x <--- tanh(y)
-    x = h[t][i] = tanh(y);
   }
   return h[t].back();
 }
@@ -137,14 +135,14 @@ Expression SimpleRNNBuilder::add_auxiliary_input(const Expression &in, const Exp
     const vector<Expression>& vars = param_vars[i];
     assert(vars.size() >= L2H + 1);
 
-    Expression y = affine_transform({vars[HB], vars[X2H], x, vars[L2H], aux});
+    if(t > 0) {
+      x = h[t][i] = tanh( affine_transform({vars[HB], vars[X2H], x, vars[L2H], aux, vars[H2H], h[t-1][i]}) );
+    } else if(h0.size() > 0) {
+      x = h[t][i] = tanh( affine_transform({vars[HB], vars[X2H], x, vars[L2H], aux, vars[H2H], h0[i]}) );
+    } else {
+      x = h[t][i] = tanh( affine_transform({vars[HB], vars[X2H], x, vars[L2H], aux}) );
+    }
 
-    if (t == 0 && h0.size() > 0)
-      y = affine_transform({y, vars[H2H], h0[i]});
-    else if (t >= 1)
-      y = affine_transform({y, vars[H2H], h[t-1][i]});
-
-    x = h[t][i] = tanh(y);
   }
   return h[t].back();
 }
