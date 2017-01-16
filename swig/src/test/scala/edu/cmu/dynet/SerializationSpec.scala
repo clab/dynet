23c4b390006fe8b0cd6fe182d7fe9d243998c1ef
Joel Grus <joelgrus@gmail.com>
2017-01-16 13:58:40 -0800

    add string serialization functions

--- swig/src/test/scala/edu/cmu/dynet/SerializationSpec.scala
+++ swig/src/test/scala/edu/cmu/dynet/SerializationSpec.scala
@@ -9,7 +9,7 @@ class SerializationSpec extends FlatSpec with Matchers {
 
   myInitialize()
 
-  "dynet" should "correctly serialize and deserialize" in {
+  "dynet" should "correctly serialize and deserialize to disk" in {
     val original = new Model()
 
     // Add parameters
@@ -25,4 +25,21 @@ class SerializationSpec extends FlatSpec with Matchers {
     load_dynet_model(path, deserialized)
     deserialized.parameter_count() shouldBe 11  // == 2 * 3 + 5
   }
+
+  "dynet" should "correctly serialize and deserialize from string" in {
+    val original = new Model()
+
+    // Add parameters
+    original.add_parameters(dim(2, 3))
+    original.add_parameters(dim(5))
+
+    // Serialize to string
+    val modelAsString = serialize_to_string(original)
+
+    // Deserialize from string
+    val deserialized = new Model()
+    deserialize_from_string(modelAsString, deserialized)
+    deserialized.parameter_count() shouldBe 11  // == 2 * 3 + 5
+  }
+
 }
