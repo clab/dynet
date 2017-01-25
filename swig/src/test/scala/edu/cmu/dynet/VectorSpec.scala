44d58f33c4bcbd9a6a90e54544ac151d51967845
Joel Grus <joelgrus@gmail.com>
2017-01-25 08:22:08 -0800

    add java.util.Collection constructors for _Vector types
    Former-commit-id: f77779f30a5dff64f1a6f59124282eb35a0ef865

---
+++ swig/src/test/scala/edu/cmu/dynet/VectorSpec.scala
@@ -0,0 +1,44 @@
+package edu.cmu.dynet
+
+import org.scalatest._
+import edu.cmu.dynet._
+import edu.cmu.dynet.dynet_swig._
+
+class VectorSpec extends FlatSpec with Matchers {
+
+  import DynetScalaHelpers._
+
+  "FloatVector" should "construct when given a Seq[Float]" in {
+    val fv = new FloatVector(Seq(2.3f, 4.5f))
+
+    fv.size shouldBe 2
+    fv.get(0) shouldBe 2.3f
+    fv.get(1) shouldBe 4.5f
+  }
+
+  "FloatVector" should "construct when given a Seq[Double]" in {
+    val fv = new FloatVector(Seq(2.3, 4.5))
+
+    fv.size shouldBe 2
+    fv.get(0) shouldBe 2.3f
+    fv.get(1) shouldBe 4.5f
+  }
+
+  "DoubleVector" should "construct when given a Seq[Double]" in {
+    val dv = new DoubleVector(Seq(2.3, 4.5, -10.2))
+
+    dv.size shouldBe 3
+    dv.get(0) shouldBe 2.3
+    dv.get(1) shouldBe 4.5
+    dv.get(2) shouldBe -10.2
+  }
+
+  "IntVector" should "construct when given a Seq[Int]" in {
+    val iv = new IntVector(Seq(23, 45, -102))
+
+    iv.size shouldBe 3
+    iv.get(0) shouldBe 23
+    iv.get(1) shouldBe 45
+    iv.get(2) shouldBe -102
+  }
+}
