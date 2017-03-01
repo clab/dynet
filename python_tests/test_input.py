fb0aa6f110adaa1498920dbe8a760edf6137e885
paul <pmichel31415@gmail.com>
2017-03-01 18:26:08 -0500

    Change expression.dim behaviour to get ((dim_1,dim_2,...,dim_n),batch_dim)
    Former-commit-id: da1e019bbed6ec94e530b3e75a6cb65912348804

--- python_tests/test_input.py
+++ python_tests/test_input.py
@@ -10,19 +10,19 @@ for i in range(4):
     dy.renew_cg()
     input_tensor = input_vals.reshape(shapes[i])
     x = dy.inputTensor(input_tensor)
-    assert (x.dim()[:-1] == shapes[i] and x.dim()[-1] == 1),"Dimension mismatch : {} : {}, {} : 1".format(x.dim()[:-1], shapes[i], x.dim()[-1])
+    assert (x.dim()[0] == shapes[i] and x.dim()[1] == 1),"Dimension mismatch : {} : ({}, {})".format(x.dim(), shapes[i],1)
     assert (x.npvalue() == input_tensor).all(), "Expression value different from initial value"
     assert dy.squared_norm(x).scalar_value() == squared_norm, "Value mismatch"
     # Batched
     dy.renew_cg()
     xb = dy.inputTensor(input_tensor, batched=True)
-    assert (xb.dim() == shapes[i] or (i == 0 and xb.dim() == (1,shapes[0][0]))), "Dimension mismatch with batch size"
+    assert (xb.dim()[0] == (shapes[i][:-1] if i>0 else (1,)) and xb.dim()[1] == shapes[i][-1]), "Dimension mismatch with batch size : {} : ({}, {})".format(xb.dim(), (shapes[i][:-1] if i>0 else 1),shapes[i][-1])
     assert (xb.npvalue() == input_tensor).all(), "Batched expression value different from initial value"
     assert dy.sum_batches(dy.squared_norm(xb)).scalar_value() == squared_norm, "Value mismatch"
     # Batched with list
     dy.renew_cg()
     xb = dy.inputTensor([np.asarray(x).transpose() for x in input_tensor.transpose()])
-    assert (xb.dim() == shapes[i] or (i == 0 and xb.dim() == (1,shapes[0][0]))), "Dimension mismatch with batch size"
+    assert (xb.dim()[0] == (shapes[i][:-1] if i>0 else (1,)) and xb.dim()[1] == shapes[i][-1]) , "Dimension mismatch with batch size : {} : ({}, {})".format(xb.dim(), (shapes[i][:-1] if i>0 else 1),shapes[i][-1])
     assert (xb.npvalue() == input_tensor).all(), "Batched expression value different from initial value"
     assert dy.sum_batches(dy.squared_norm(xb)).scalar_value() == squared_norm, "Value mismatch"
 
