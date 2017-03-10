efc14d42a27d0e5024dfea51f40c860f8904a041
Joel Grus <joelgrus@gmail.com>
2017-03-10 11:26:23 -0800

    add missing bindings, rename RichNumeric
    Former-commit-id: b7e74f9f505586049bac40bcd3b1f97427c1b6eb

--- contrib/swig/src/main/scala/edu/cmu/dynet/Tensor.scala
+++ contrib/swig/src/main/scala/edu/cmu/dynet/Tensor.scala
@@ -7,5 +7,6 @@ class Tensor private[dynet] (private[dynet] val tensor: internal.Tensor) {
   def toFloat(): Float = internal.dynet_swig.as_scalar(tensor)
   def toVector(): FloatVector = new FloatVector(internal.dynet_swig.as_vector(tensor))
   def toSeq(): Seq[Float] = toVector()
+  def getD(): Dim = new Dim(tensor.getD)
 }
 
