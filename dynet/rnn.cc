fab7d52d2ab3881e3636507dafbcaaf37df2d76b
xunzhang <xunzhangthu@gmail.com>
2017-03-22 22:35:14 -0400

    Add simple get_parameters for LSTMBuilder and SimpleRNNBuilder.
    Former-commit-id: 4204c3d7b2fe8a81e76034320054efb8dc96b38d

--- dynet/rnn.cc
+++ dynet/rnn.cc
@@ -34,14 +34,15 @@ SimpleRNNBuilder::SimpleRNNBuilder(unsigned layers,
                        unsigned hidden_dim,
                        ParameterCollection& model,
                        bool support_lags) : layers(layers), lagging(support_lags) {
+  auto local_model = model.add_subcollection("simple-rnn-builder");
   unsigned layer_input_dim = input_dim;
   for (unsigned i = 0; i < layers; ++i) {
-    Parameter p_x2h = model.add_parameters({hidden_dim, layer_input_dim});
-    Parameter p_h2h = model.add_parameters({hidden_dim, hidden_dim});
-    Parameter p_hb = model.add_parameters({hidden_dim});
+    Parameter p_x2h = local_model.add_parameters({hidden_dim, layer_input_dim});
+    Parameter p_h2h = local_model.add_parameters({hidden_dim, hidden_dim});
+    Parameter p_hb = local_model.add_parameters({hidden_dim});
     vector<Parameter> ps = {p_x2h, p_h2h, p_hb};
     if (lagging)
-        ps.push_back(model.add_parameters({hidden_dim, hidden_dim}));
+        ps.push_back(local_model.add_parameters({hidden_dim, hidden_dim}));
     params.push_back(ps);
     layer_input_dim = hidden_dim;
   }
