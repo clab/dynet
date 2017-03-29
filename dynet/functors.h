0b82ac43502fd9edebd8d7db2a2aa8ab33719e44
Paul Michel <pmichel31415@gmail.Com>
2017-03-28 21:49:34 -0400

    Fix numerical issue in binary log loss

--- dynet/functors.h
+++ dynet/functors.h
@@ -291,12 +291,12 @@ struct FBinaryLogLoss {
       return -1.f * x_true * log(x);
     }
     else if (x_true == 0.f) {
-      if (x == 1.f) x = DYNET_DEVICE_MIN;
+      if (x == 1.f) x -= DYNET_DEVICE_MIN;
       return (x_true - 1.f) * log1p(-x);
     }
     else {
       if (x == 0.f) x = DYNET_DEVICE_MIN;
-      if (x == 1.f) x = DYNET_DEVICE_MIN;
+      if (x == 1.f) x -= DYNET_DEVICE_MIN;
       return -1.f * (x_true * log(x) + (1.f - x_true) * log1p(-x));
     }
   }
