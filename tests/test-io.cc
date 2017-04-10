c5dd43c332f8857ff8614f53578d0ef458e59b45
xunzhang <xunzhangthu@gmail.com>
2017-04-10 11:08:37 -0400

    Refactor test case for save/load function.
    Former-commit-id: 190e7b45e47159f44fa254ed736faccf3213f989

--- tests/test-io.cc
+++ tests/test-io.cc
@@ -1,17 +1,21 @@
 #define BOOST_TEST_MODULE TEST_IO
 
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+
+#include <boost/test/unit_test.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+
 #include <dynet/dynet.h>
 #include <dynet/expr.h>
 #include <dynet/rnn.h>
 #include <dynet/lstm.h>
 #include <dynet/gru.h>
-#include <boost/test/unit_test.hpp>
-#include <boost/archive/text_iarchive.hpp>
-#include <boost/archive/text_oarchive.hpp>
-#include <iostream>
-#include <fstream>
+#include <dynet/io.h>
 
-#include <stdexcept>
+#include "test.h"
 
 using namespace dynet;
 using namespace dynet::expr;
@@ -37,6 +41,25 @@ struct IOTest {
     std::vector<char*> av;
     std::string filename;
 };
+
+class testModel {
+ public:
+  testModel(dynet::ParameterCollection &model) {
+    lookup_param = model.add_lookup_parameters(1000, {128});
+    affine_params = model.add_subcollection("affine");
+    W_x = affine_params.add_parameters({40, 30});
+    b_x = affine_params.add_parameters({40});
+    lstm = LSTMBuilder(3, 40, 1, model);
+  }
+  std::string get_affine_model_name() { return affine_params.get_namespace(); }
+  dynet::ParameterCollection get_affine_model() const { return affine_params; }
+  dynet::ParameterCollection get_lstm_model() { return lstm.get_parameters(); }
+ private:
+  dynet::LookupParameter lookup_param;
+  dynet::Parameter W_x, b_x;
+  dynet::ParameterCollection affine_params;
+  dynet::LSTMBuilder lstm;
+}; // class testModel
 
 // define the test suite
 BOOST_FIXTURE_TEST_SUITE(io_test, IOTest);
@@ -104,5 +127,68 @@ BOOST_AUTO_TEST_CASE( lstm_io ) {
     BOOST_CHECK(rnn2.hid == 10);
 }
 
+BOOST_AUTO_TEST_CASE ( test_save_load_parameter_collection ) {
+  {
+    ParameterCollection m;
+    Parameter a = m.add_parameters({10}, "a");
+    Parameter b = m.add_parameters({3,7});
+    LookupParameter c = m.add_lookup_parameters(10, {2});
+    std::remove("test.model"); std::remove("test.model.meta");
+    dynet::Pack s("test.model");
+    s.save(m, "model1");
+    s.save(m, m.get_namespace(), true);
+
+    ParameterCollection m2;
+    s.load(m2, "model1");
+    DYNET_CHECK_EQUAL(m2, m);
+    ParameterCollection m3;
+    s.load(m3, "/");
+  }
+  {
+    ParameterCollection collec;
+    testModel spec(collec);
+    std::remove("a.model"); std::remove("a.model.meta");
+    Pack s1("a.model");
+    s1.save(collec, "all");
+    ParameterCollection collec2;
+    s1.load(collec2);
+    DYNET_CHECK_EQUAL(collec2.size(), collec.size());
+  
+    std::remove("b.model"); std::remove("b.model.meta");
+    Pack s2("b.model");
+    s2.save(collec, "all");
+    s2.save(spec.get_lstm_model(), "lstm", true);
+    s2.save(spec.get_affine_model(), "affine", true);
+    ParameterCollection collec3, lstm2, affine2;
+    s2.load(affine2, "affine");
+    s2.load(collec3, "all");
+    s2.load(lstm2, "lstm");
+    DYNET_CHECK_EQUAL(affine2.size(), spec.get_affine_model().size());
+    DYNET_CHECK_EQUAL(collec3.size(), collec.size());
+    DYNET_CHECK_EQUAL(lstm2.size(), spec.get_lstm_model().size());
+
+    std::remove("c.model"); std::remove("c.model.meta");
+    s2.reinit("c.model");
+    s2.save(lstm2, "lstm");
+    s2.save(collec3, "all", true);
+    s2.save(affine2, "affine", true);
+  }
+  {
+    ParameterCollection cc;
+    auto cc2 = cc.add_subcollection("xx");
+    cc2.add_parameters({10});
+    std::remove("d.model"); std::remove("d.model.meta");
+    Pack s3("d.model");
+    s3.save(cc, "key");
+
+    ParameterCollection ccc;
+    s3.load(ccc, "key");
+    DYNET_CHECK_EQUAL(ccc.size(), cc.size());
+
+    std::remove("e.model"); std::remove("e.model.meta");
+    s3.reinit("e.model");
+    s3.save(ccc);
+  }
+}
 
 BOOST_AUTO_TEST_SUITE_END()
