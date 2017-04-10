190e7b45e47159f44fa254ed736faccf3213f989
xunzhang <xunzhangthu@gmail.com>
2017-04-10 11:08:37 -0400

    Refactor test case for save/load function.

--- tests/test-params.cc
+++ tests/test-params.cc
@@ -66,25 +66,6 @@ class testModel {
   dynet::ParameterCollection affine_params;
 }; // class testModel
 
-class testModel2 {
- public:
-  testModel2(dynet::ParameterCollection &model) {
-    lookup_param = model.add_lookup_parameters(1000, {128});
-    affine_params = model.add_subcollection("affine");
-    W_x = affine_params.add_parameters({40, 30});
-    b_x = affine_params.add_parameters({40});
-    lstm = LSTMBuilder(3, 40, 1, model);
-  }
-  std::string get_affine_model_name() { return affine_params.get_namespace(); }
-  dynet::ParameterCollection get_affine_model() const { return affine_params; }
-  dynet::ParameterCollection get_lstm_model() { return lstm.get_parameters(); }
- private:
-  dynet::LookupParameter lookup_param;
-  dynet::Parameter W_x, b_x;
-  dynet::ParameterCollection affine_params;
-  dynet::LSTMBuilder lstm;
-}; // class testModel2
-
 // define the test suite
 BOOST_FIXTURE_TEST_SUITE(params_test, ParamsTest);
 
@@ -161,20 +142,6 @@ BOOST_AUTO_TEST_CASE ( test_parameter_class ) {
                                             affine_model.get_parameter_storage("/affine__0/__0")), "/affine__0/__0");
 }
 
-BOOST_AUTO_TEST_CASE ( test_parameter_class_with_builder ) {
-  auto save_parameters_lambda = [] (const std::string & fname,
-                                    ParameterCollection & param_list) -> size_t {
-     for (auto & param : param_list.get_parameter_storages()) {
-       std::cout << param->name << " saved in file " << fname << std::endl;
-     }
-     return param_list.size();
-  };
-  ParameterCollection collec;
-  testModel2 spec(collec);
-  auto params = spec.get_lstm_model();
-  save_parameters_lambda("lstm_file.txt", params);
-}
-
 BOOST_AUTO_TEST_CASE ( test_parametercollection_with_builder ) {
   dynet::ParameterCollection collec;
   auto gru_builder = dynet::GRUBuilder(3, 10, 2, collec);
@@ -182,112 +149,6 @@ BOOST_AUTO_TEST_CASE ( test_parametercollection_with_builder ) {
   dynet::ParameterCollection collec2;
   auto bi_treelstm_builder = BidirectionalTreeLSTMBuilder(3, 10, 2, collec2);
   DYNET_CHECK_EQUAL(bi_treelstm_builder.get_parameters().size(), 11 * 3 * 2);
-}
-
-BOOST_AUTO_TEST_CASE ( test_save_load_parameter_collection ) {
-  ParameterCollection m;
-  Parameter a = m.add_parameters({10}, "a");
-  Parameter b = m.add_parameters({3,7});
-  LookupParameter c = m.add_lookup_parameters(10, {2});
-  std::remove("test.model"); std::remove("test.model.meta");
-  dynet::Pack s("test.model");
-  s.save(m, "model1");
-  s.save(m, m.get_namespace(), true);
-
-  ParameterCollection m2;
-  s.load(m2, "model1");
-  auto params1 = m2.get_parameter_storages();
-  for(auto & x : params1) {
-    std::cout << x->name << std::endl;
-    std::cout << x->dim << std::endl;
-    std::cout << x->values << std::endl;
-    std::cout << x->g << std::endl;
-  }
-  auto params11 = m.get_parameter_storages();
-  for(auto & x : params11) {
-    std::cout << x->name << std::endl;
-    std::cout << x->dim << std::endl;
-    std::cout << x->values << std::endl;
-    std::cout << x->g << std::endl;
-  }
-  ParameterCollection m3;
-  s.load(m3, "/");
-  auto params2 = m3.get_parameter_storages();
-  for(auto & x : params2) {
-    std::cout << x->name << std::endl;
-    std::cout << x->dim << std::endl;
-    std::cout << x->values << std::endl;
-    std::cout << x->g << std::endl;
-  }
-  auto lookup_params = m2.get_lookup_parameter_storages();
-  for(auto & x : lookup_params) {
-    std::cout << x->name << std::endl;
-    std::cout << x->dim << std::endl;
-    std::cout << x->all_dim << std::endl;
-    std::cout << x->all_values << std::endl;
-    std::cout << x->all_grads << std::endl;
-    std::cout << x->values[0] << std::endl;
-    std::cout << x->grads[0] << std::endl;
-  }
-  auto lookup_paramss = m.get_lookup_parameter_storages();
-  for(auto & x : lookup_paramss) {
-    std::cout << x->name << std::endl;
-    std::cout << x->dim << std::endl;
-    std::cout << x->all_dim << std::endl;
-    std::cout << x->all_values << std::endl;
-    std::cout << x->all_grads << std::endl;
-    std::cout << x->values[0] << std::endl;
-    std::cout << x->grads[0] << std::endl;
-  }
-}
-
-BOOST_AUTO_TEST_CASE ( test_save_load_parameter_collections ) {
-  {
-    ParameterCollection collec;
-    testModel2 spec(collec);
-    std::remove("a.model"); std::remove("a.model.meta");
-    Pack s1("a.model");
-    s1.save(collec, "all");
-    ParameterCollection collec2;
-    s1.load(collec2);
-    DYNET_CHECK_EQUAL(collec2.size(), collec.size());
-  
-    std::remove("b.model"); std::remove("b.model.meta");
-    Pack s2("b.model");
-    s2.save(collec, "all");
-    s2.save(spec.get_lstm_model(), "lstm", true);
-    s2.save(spec.get_affine_model(), "affine", true);
-    ParameterCollection collec3, lstm2, affine2;
-    s2.load(affine2, "affine");
-    s2.load(collec3, "all");
-    s2.load(lstm2, "lstm");
-    DYNET_CHECK_EQUAL(affine2.size(), spec.get_affine_model().size());
-    DYNET_CHECK_EQUAL(collec3.size(), collec.size());
-    DYNET_CHECK_EQUAL(lstm2.size(), spec.get_lstm_model().size());
-
-    std::remove("c.model"); std::remove("c.model.meta");
-    s2.reinit("c.model");
-    s2.save(lstm2, "lstm");
-    s2.save(collec3, "all", true);
-    s2.save(affine2, "affine", true);
-  }
-
-  {
-    ParameterCollection cc;
-    auto cc2 = cc.add_subcollection("xx");
-    cc2.add_parameters({10});
-    std::remove("d.model"); std::remove("d.model.meta");
-    Pack s3("d.model");
-    s3.save(cc, "key");
-
-    ParameterCollection ccc;
-    s3.load(ccc, "key");
-    DYNET_CHECK_EQUAL(ccc.size(), cc.size());
-
-    std::remove("e.model"); std::remove("e.model.meta");
-    s3.reinit("e.model");
-    s3.save(ccc);
-  }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
--- tests/test.h
+++ tests/test.h
@@ -4,34 +4,13 @@
 #include <vector>
 #include <unordered_map>
 #include <boost/test/unit_test.hpp>
+#include <dynet/model.h>
 
-#define TOL 1e-7
+#define TOL 1e-3
 #define DYNET_CHECK_EQUAL(a, b) equal_check(a, b)
 #define DYNET_CHECK_CLOSE(a, b) close_check(a, b)
 
-template <class T, class U>
-void equal_check(T a, U b) {
-  BOOST_CHECK_EQUAL(a, b);
-}
-
-template <class T>
-void equal_check(std::vector<T> a,
-                 std::vector<T> b) {
-  BOOST_CHECK_EQUAL(a.size(), b.size());
-  for(size_t i = 0; i < a.size(); ++i) {
-    BOOST_CHECK_EQUAL(a[i], b[i]);
-  }
-}
-
-template <class K, class V>
-void equal_check(std::unordered_map<K, V> a,
-                 std::unordered_map<K, V> b) {
-  BOOST_CHECK_EQUAL(a.size(), b.size());
-  for(auto & kv : a) {
-    BOOST_CHECK(b.count(kv.first));
-    BOOST_CHECK_EQUAL(kv.second, b[kv.first]);
-  }
-}
+using namespace dynet;
 
 template <class T>
 void close_check(T a, T b) {
@@ -54,6 +33,72 @@ void close_check(std::unordered_map<K, V> a,
   for(auto & kv : a) {
     BOOST_CHECK(b.count(kv.first));
     BOOST_CHECK_CLOSE(kv.second, b[kv.first], TOL);
+  }
+}
+
+void equal_check(Tensor & v1, Tensor & v2) {
+  std::vector<real> lv1 = dynet::as_vector(v1);
+  std::vector<real> lv2 = dynet::as_vector(v2);
+  BOOST_CHECK_EQUAL(lv1.size(), lv2.size());
+  for (size_t i = 0; i < lv1.size(); ++i) {
+    DYNET_CHECK_CLOSE(lv1[i], lv2[i]);
+  }
+}
+
+template <class T>
+void equal_check(std::vector<T> & a,
+                 std::vector<T> & b) {
+  BOOST_CHECK_EQUAL(a.size(), b.size());
+  for(size_t i = 0; i < a.size(); ++i) {
+    BOOST_CHECK_EQUAL(a[i], b[i]);
+  }
+}
+
+void equal_check(ParameterStorage *p1,
+                 ParameterStorage *p2) {
+  BOOST_CHECK_EQUAL(p1->name, p2->name);
+  BOOST_CHECK_EQUAL(p1->dim, p2->dim);
+  equal_check(p1->values, p2->values);
+  equal_check(p1->g, p2->g);
+}
+
+void equal_check(LookupParameterStorage *p1,
+                 LookupParameterStorage *p2) {
+  BOOST_CHECK_EQUAL(p1->name, p2->name);
+  BOOST_CHECK_EQUAL(p1->all_dim, p2->all_dim);
+  BOOST_CHECK_EQUAL(p1->dim, p2->dim);
+  equal_check(p1->all_values, p2->all_values);
+  equal_check(p1->all_grads, p2->all_grads);
+}
+
+template <class T, class U>
+void equal_check(T a, U b) {
+  BOOST_CHECK_EQUAL(a, b);
+}
+
+void equal_check(ParameterCollection & model1,
+                 ParameterCollection & model2) {
+  auto params1 = model1.get_parameter_storages();
+  auto params2 = model2.get_parameter_storages();
+  BOOST_CHECK_EQUAL(params1.size(), params2.size());
+  for (size_t i = 0; i < params1.size(); ++i) {
+    equal_check(params1[i], params2[i]);
+  }
+  auto lookup_params1 = model1.get_lookup_parameter_storages();
+  auto lookup_params2 = model2.get_lookup_parameter_storages();
+  BOOST_CHECK_EQUAL(lookup_params1.size(), lookup_params2.size());
+  for (size_t i = 0; i < lookup_params1.size(); ++i) {
+    equal_check(lookup_params1[i], lookup_params2[i]);
+  }
+}
+
+template <class K, class V>
+void equal_check(std::unordered_map<K, V> a,
+                 std::unordered_map<K, V> b) {
+  BOOST_CHECK_EQUAL(a.size(), b.size());
+  for(auto & kv : a) {
+    BOOST_CHECK(b.count(kv.first));
+    BOOST_CHECK_EQUAL(kv.second, b[kv.first]);
   }
 }
 
