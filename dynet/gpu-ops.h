46bdee5d52e0beb603efa99d2e06e4d8b715e051
Hao Zhang <zhisbug@gmail.com>
2017-04-11 11:20:35 -0400

    change cudnn-ops.cc to cudnn-ops.cu

--- dynet/gpu-ops.h
+++ dynet/gpu-ops.h
@@ -34,7 +34,7 @@ void sparse_to_dense_assign(int n, const unsigned int* ids, float* src, float* t
 void dense_to_sparse_subtract(int n, const unsigned int* ids, float* src, float* trg);
 void sparse_to_dense_block_assign_and_multiply(int n, const unsigned *idx, int bsize, float mult, float *src, float *trg);
 void dense_to_sparse_block_add(int n, const unsigned* ids, int bsize, float* src, float* trg);
-void pad_input(float* output, const float* input, int N, int C, int H, int W, int pad_right, int pad_bottom);
+//void pad_input(float* output, const float* input, int N, int C, int H, int W, int pad_right, int pad_bottom);
 
 } // namespace gpu
 } // namespace dynet
