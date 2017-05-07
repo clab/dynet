6b6b3207184bd65ab96961b3a13bf84147e89131
Graham Neubig <neubig@gmail.com>
2017-05-07 10:55:14 -0400

    Not-working version of pickneglogsoftmax

--- dynet/nodes.h
+++ dynet/nodes.h
@@ -639,6 +639,17 @@ struct PickNegLogSoftmax : public Node {
   DYNET_NODE_DEFINE_DEV_IMPL()
   virtual bool supports_multibatch() const override { return true; }
   size_t aux_storage_size() const override;
+  virtual std::string autobatch_profile(const ComputationGraph & cg) const override;
+  virtual std::vector<bool> autobatch_concat(const ComputationGraph & cg) const override;
+  virtual Node* autobatch_pseudo_node(const ComputationGraph & cg,
+                                      const std::vector<VariableIndex> & batch_ids) const override;
+  virtual void autobatch_reshape(const ComputationGraph & cg,
+                                 const std::vector<VariableIndex> & batch_ids,
+                                 const std::vector<bool> & concat,
+                                 std::vector<const Tensor*>& xs,
+                                 Tensor& fx) const override {
+    autobatch_reshape_concatonly(cg, batch_ids, concat, xs, fx);
+  }
   unsigned val;
   const unsigned* pval;
   std::vector<unsigned> vals;
