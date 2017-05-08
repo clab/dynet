2a1dc3d4613b2c8452790eb390770bd89577920c
Graham Neubig <neubig@gmail.com>
2017-05-08 17:37:23 -0400

    Added hash and tree
    Former-commit-id: 4f84c69f7f6fa37673d0c5f4e34343937823f54b

--- dynet/sig.h
+++ dynet/sig.h
@@ -5,9 +5,10 @@
 
 namespace dynet {
 
-struct Sig {
-  Sig(int which) : tail(data+1) { data[0] = which; }
-  Sig() : tail(data+1) { data[0] = 0; }
+struct SigString {
+  SigString(int which) : which(which), tail(data) { }
+  SigString() : which(which), tail(data) { }
+  int which;
   int data[DYNET_MAX_SIG];
   int* tail;
 
@@ -20,16 +21,53 @@ struct Sig {
 
 };
 
-inline bool operator==(const Sig& a, const Sig& b) {
+inline bool operator<(const SigString& a, const SigString& b) {
+  auto c = cmp(a.which, b.which);
+  if(c != 0) return c < 0;
+  ptrdiff_t a_size = a.tail - a.data;
+  c = cmp(a_size, (ptrdiff_t)(b.tail - b.data));
+  if(c != 0) return c < 0;
+  return memcmp(a.data, b.data, a_size * sizeof(int)) < 0;
+}
+
+inline bool operator==(const SigString& a, const SigString& b) {
+  if(a.which != b.which) return false;
   ptrdiff_t a_size = a.tail - a.data;
   if(a_size != (ptrdiff_t)(b.tail - b.data)) return false;
   return memcmp(a.data, b.data, a_size * sizeof(int)) == 0;
 }
 
-inline bool operator!=(const Sig& a, const Sig& b) { return !(a == b); }
+inline bool operator!=(const SigString& a, const SigString& b) { return !(a == b); }
 
-struct SigMap {
-  SigMap() { sigs.resize(50); }
+struct SigHash {
+  SigHash(int which) : value(0xcc9e2d51 ^ which) { }
+  SigHash() : value(0xcc9e2d51) { }
+  int hash;
+
+  // sbdm hash
+  inline void add_int(int i) {
+    value = i + (hash << 6) + (hash << 16) - hash;
+  }
+  void add_node(VariableIndex i) { add_int((int)i); }
+  void add_dim(const Dim &d) {
+    add_int(-(int)d.nd);
+    for(size_t i = 0; < d.nd; ++i)
+      add_int((int)d.d[i]);
+  }
+
+};
+
+inline bool operator<(const SigHash& a, const SigHash& b) {
+  return a.value < b.value;
+}
+inline bool operator==(const SigHash& a, const SigHash& b) {
+  return a.value == b.value;
+}
+inline bool operator!=(const SigHash& a, const SigHash& b) { return a.value != b.value; }
+
+template <class Sig>
+struct SigLinearMap {
+  SigLinearMap() { sigs.resize(50); }
   int get_idx(Sig &s) {
     for (int i=0; i<sigs.size(); ++i) {
       if (sigs[i]==s) return i;
@@ -38,6 +76,18 @@ struct SigMap {
     return sigs.size()-1;
   }
   std::vector<Sig> sigs;
+};
+
+template <class Sig>
+struct SigTreeMap {
+  SigTreeMap() { }
+  int get_idx(Sig &s) {
+    auto it = sigs.find(s);
+    if(it != sigs.end()) return sigs->second;
+    sigs.insert(make_pair(s, (int)sigs.size()));
+    return sigs.size()-1;
+  }
+  std::map<Sig, int> sigs;
 };
 
 // struct Sig {
