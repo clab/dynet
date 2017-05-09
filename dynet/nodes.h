90512cad190866d12be227d6fefc532c06c5cf5f
paul <pmichel31415@gmail.com>
2017-05-09 11:25:41 -0400

    Dropout batch elements
    Former-commit-id: 7a4c780ea97f68ad61f124fbacb070689d46c205

--- dynet/nodes.h
+++ dynet/nodes.h
@@ -162,6 +162,15 @@ struct DropoutDim : public Node {
   real p;
 };
 
+// y = dropout(x,p) where p specifies the dropout probability
+struct DropoutBatch : public Node {
+  explicit DropoutBatch(const std::initializer_list<VariableIndex>& a, real p) : Node(a), p(p) {}
+  DYNET_NODE_DEFINE_DEV_IMPL()
+  size_t aux_storage_size() const override;
+  virtual bool supports_multibatch() const override { return true; }
+  real p;
+};
+
 // y = block_dropout(x,p) where p specifies the probability for dropping-out the entire block
 struct BlockDropout : public Node {
   explicit BlockDropout(const std::initializer_list<VariableIndex>& a, real p) : Node(a), dropout_probability(p) {}
