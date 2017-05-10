bd481690ac7b5ac3be79c2cfae41250db47ed527
paul <pmichel31415@gmail.com>
2017-05-10 12:57:54 -0400

    Add weight normalization

--- dynet/nodes.cc
+++ dynet/nodes.cc
@@ -2823,4 +2823,31 @@ void MinDimension::backward_dev_impl(const MyDevice & dev,
 }
 DYNET_NODE_INST_DEV_IMPL(MinDimension)
 
+template<class MyDevice>
+void WeightNormalization::forward_dev_impl(const MyDevice & dev, const vector<const Tensor*>& xs, Tensor& fx) const {
+  DYNET_ASSERT(xs.size() == 2, "Failed dimension check in WeightNormalization::forward");
+  Eigen::array<ptrdiff_t, 1> red_axis = {0};
+  Eigen::array<ptrdiff_t, 1> bcast = {xs[0]->d.size()};
+  Eigen::array<ptrdiff_t, 1> morph = {1};
+  fx.tvec().device(*dev.edevice) = (xs[0]->tvec() / xs[0]->tvec().square().sum(red_axis).sqrt().reshape(morph).broadcast(bcast)) * as_scalar(*xs[1]);
+}
+
+template<class MyDevice>
+void WeightNormalization::backward_dev_impl(const MyDevice & dev,
+                             const vector<const Tensor*>& xs,
+                             const Tensor& fx,
+                             const Tensor& dEdf,
+                             unsigned i,
+                             Tensor& dEdxi) const {
+  Eigen::array<ptrdiff_t, 1> red_axis = {0};
+  Eigen::array<ptrdiff_t, 1> bcast = {xs[0]->d.size()};
+  Eigen::array<ptrdiff_t, 1> morph = {1};
+  if (i==0){
+    dEdxi.tvec().device(*dev.edevice) += (dEdf.tvec() / xs[0]->tvec().square().sum(red_axis).sqrt().reshape(morph).broadcast(bcast)) * as_scalar(*xs[1]) - fx.tvec() * (((dEdf.tvec() * xs[0]->tvec()).sum(red_axis)) / xs[0]->tvec().square().sum(red_axis)).reshape(morph).broadcast(bcast);
+  }else{
+    dEdxi.t<0>().device(*dev.edevice) += ((dEdf.tvec() * xs[0]->tvec()).sum(red_axis)) /  xs[0]->tvec().square().sum(red_axis).sqrt();
+  }
+}
+DYNET_NODE_INST_DEV_IMPL(WeightNormalization)
+
 } // namespace dynet
