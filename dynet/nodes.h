93a56e3737a8a3c8cb0ec372c71882aae9418d62
Graham Neubig <neubig@gmail.com>
2017-05-29 16:07:02 -0400

    Fixed bug in autobatched sum w/ manual batching
    Former-commit-id: d1bbb99caf56fd31e6e059e353902dcc559da7b0

--- dynet/nodes.h
+++ dynet/nodes.h
@@ -491,7 +491,15 @@ struct LogDet : public Node {
 struct Sum : public Node {
   template <typename T> explicit Sum(const T& a) : Node(a) {}
   virtual int autobatch_sig(const ComputationGraph &cg, SigMap &sm) const override;
-  virtual std::vector<int> autobatch_concat(const ComputationGraph & cg) const override { return std::vector<int>(args.size(), 1);}
+  virtual std::vector<int> autobatch_concat(const ComputationGraph & cg) const override;
+  virtual void autobatch_reshape(const ComputationGraph & cg,
+                                 const std::vector<VariableIndex> & batch_ids,
+                                 const std::vector<int> & concat,
+                                 std::vector<const Tensor*>& xs,
+                                 Tensor& fx) const override {
+    if(dim.bd != 1)
+      autobatch_reshape_concatonly(cg, batch_ids, concat, xs, fx);
+  }
   DYNET_NODE_DEFINE_DEV_IMPL()
   virtual bool supports_multibatch() const override { return true; }
 };
