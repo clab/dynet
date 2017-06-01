e8b69e0e9739c6a92d0c830036f329eb1f13c374
xunzhang <xunzhang@apache.org>
2017-06-01 13:23:58 +0800

    add byte count for save/load.

--- dynet/io.cc
+++ dynet/io.cc
@@ -37,33 +37,40 @@ void TextFileSaver::save(const LookupParameter & param,
 
 void TextFileSaver::save(const ParameterStorage & p,
                          const string & key) {
-  datastream << "#Parameter# " << (key.size() > 0 ? key : p.name) << ' ' << p.dim << endl;
-  datastream << dynet::as_vector(p.values) << endl;
-  datastream << dynet::as_vector(p.g) << endl;
+  std::ostringstream buffer;
+  buffer << dynet::as_vector(p.values) << endl;
+  buffer << dynet::as_vector(p.g) << endl;
+  datastream << "#Parameter# " << (key.size() > 0 ? key : p.name) << ' ' << p.dim << ' ' << buffer.str().size() << endl;
+  datastream.write(buffer.str().c_str(), buffer.str().size());
 }
 
 void TextFileSaver::save(const LookupParameterStorage & p,
                          const string & key) {
-  datastream << "#LookupParameter# " << (key.size() > 0 ? key : p.name) << ' ' << p.all_dim << ' ' << endl;
-  datastream << dynet::as_vector(p.all_values) << endl;
-  datastream << dynet::as_vector(p.all_grads) << endl;
+  std::ostringstream buffer;
+  buffer << dynet::as_vector(p.all_values) << endl;
+  buffer << dynet::as_vector(p.all_grads) << endl;
+  datastream << "#LookupParameter# " << (key.size() > 0 ? key : p.name) << ' ' << p.all_dim << ' ' << buffer.str().size() << endl;
+  datastream.write(buffer.str().c_str(), buffer.str().size());
 }
+
+TextFileLoader::TextFileLoader(const string & filename) :
+        dataname(filename) { }
 
 void TextFileLoader::populate(ParameterCollection & model, const string & key) {
   ifstream datastream(dataname);
   if(!datastream) DYNET_RUNTIME_ERR("Could not read model from " << dataname);
   string line, type, name;
   Dim dim;
+  size_t byte_count = 0;
   vector<float> values;
   Tensor *value_t, *grad_t;
   size_t param_id = 0, lookup_id = 0;
   ParameterCollectionStorage & storage = model.get_storage();
   while(getline(datastream, line)) {
-    { istringstream iss(line); iss >> type >> name >> dim; }
+    { istringstream iss(line); iss >> type >> name >> dim >> byte_count; }
     // Skip ones that don't match
     if(key.size() != 0 && name.substr(0, key.size()) != key) {
-      getline(datastream, line);
-      getline(datastream, line);
+      datastream.seekg(datastream.tellg() + byte_count);
       continue;
     // Load a parameter
     } else if(type == "#Parameter#") {
@@ -99,7 +106,6 @@ void TextFileLoader::populate(ParameterCollection & model, const string & key) {
     DYNET_RUNTIME_ERR("Number of parameter/lookup parameter objects loaded from file (" << 
                       param_id << '/' << lookup_id << ") did not match number to be populated (" <<
                       storage.params.size() << '/' << storage.lookup_params.size() << ')');
-
 }
 
 void TextFileLoader::populate(Parameter & param,
@@ -110,8 +116,9 @@ void TextFileLoader::populate(Parameter & param,
   if(!datastream) DYNET_RUNTIME_ERR("Could not read model from " << dataname);
   string line, type, name;
   Dim dim;
+  size_t byte_count = 0;
   while(getline(datastream, line)) {
-    { istringstream iss(line); iss >> type >> name >> dim; }
+    { istringstream iss(line); iss >> type >> name >> dim >> byte_count; }
     if(type == "#Parameter#" && name == key) {
       if(param.p->dim != dim)
         DYNET_RUNTIME_ERR("Attempted to populate parameter where arguments don't match (" << param.p->dim << " != " << dim << ")");
@@ -122,8 +129,7 @@ void TextFileLoader::populate(Parameter & param,
       TensorTools::set_elements(param.get_storage().g, values);
       return;
     } else {
-      getline(datastream, line);
-      getline(datastream, line);
+      datastream.seekg(datastream.tellg() + byte_count);
     }
   }
   DYNET_RUNTIME_ERR("Could not find key " << key << " in the model file");
@@ -137,8 +143,9 @@ void TextFileLoader::populate(LookupParameter & lookup_param,
   if(!datastream) DYNET_RUNTIME_ERR("Could not read model from " << dataname);
   string line, type, name;
   Dim dim;
+  size_t byte_count = 0;
   while(getline(datastream, line)) {
-    { istringstream iss(line); iss >> type >> name >> dim; }
+    { istringstream iss(line); iss >> type >> name >> dim >> byte_count; }
     if(type == "#LookupParameter#" && name == key) {
       if(lookup_param.p->all_dim != dim)
         DYNET_RUNTIME_ERR("Attempted to populate lookup parameter where arguments don't match (" << lookup_param.p->all_dim << " != " << dim << ")");
@@ -149,8 +156,7 @@ void TextFileLoader::populate(LookupParameter & lookup_param,
       TensorTools::set_elements(lookup_param.get_storage().all_grads, values);
       return;
     } else {
-      getline(datastream, line);
-      getline(datastream, line);
+      datastream.seekg(datastream.tellg() + byte_count);
     }
   }
   DYNET_RUNTIME_ERR("Could not find key " << key << " in the model file");
@@ -164,6 +170,7 @@ Parameter TextFileLoader::load_param(ParameterCollection & model,
   if(!datastream) DYNET_RUNTIME_ERR("Could not read model from " << dataname);
   string line, type, name;
   Dim dim;
+  size_t byte_count = 0;
   while(getline(datastream, line)) {
     { istringstream iss(line); iss >> type >> name >> dim; }
     if(type == "#Parameter#" && name == key) {
@@ -176,12 +183,10 @@ Parameter TextFileLoader::load_param(ParameterCollection & model,
       TensorTools::set_elements(param.get_storage().g, values);
       return param;
     } else {
-      getline(datastream, line);
-      getline(datastream, line);
+      datastream.seekg(datastream.tellg() + byte_count);
     }
   }
   DYNET_RUNTIME_ERR("Could not find key " << key << " in the model file");
-  return this->deserialize_param(model, key);
 }
 
 LookupParameter TextFileLoader::load_lookup_param(ParameterCollection & model,
@@ -192,6 +197,7 @@ LookupParameter TextFileLoader::load_lookup_param(ParameterCollection & model,
   if(!datastream) DYNET_RUNTIME_ERR("Could not read model from " << dataname);
   string line, type, name;
   Dim dim;
+  size_t byte_count = 0;
   while(getline(datastream, line)) {
     { istringstream iss(line); iss >> type >> name >> dim; }
     if(type == "#LookupParameter#" && name == key) {
@@ -205,12 +211,8 @@ LookupParameter TextFileLoader::load_lookup_param(ParameterCollection & model,
       TensorTools::set_elements(lookup_param.get_storage().all_grads, values);
       return lookup_param;
     } else {
-      getline(datastream, line);
-      getline(datastream, line);
+      datastream.seekg(datastream.tellg() + byte_count);
     }
   }
   DYNET_RUNTIME_ERR("Could not find key " << key << " in the model file");
 }
-
-TextFileLoader::TextFileLoader(const string & filename) :
-        dataname(filename) { }
