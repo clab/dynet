24099aef8dafe9cbd66337e64b188e9ca0096be0
shuhei-k <shuhei-k@outlook.jp>
2017-06-09 09:55:18 +0900

    Expression.layerNorm/weightNorm
    Former-commit-id: 4874f1dc7f1b1522299bd7ce066bc656f69313f7

--- contrib/swig/src/main/scala/edu/cmu/dynet/LSTMBuilder.scala
+++ contrib/swig/src/main/scala/edu/cmu/dynet/LSTMBuilder.scala
@@ -5,8 +5,8 @@ class VanillaLstmBuilder private[dynet](private[dynet] val builder: internal.Van
 
   def this() { this(new internal.VanillaLSTMBuilder()) }
 
-  def this(layers: Long, inputDim: Long, hiddenDim: Long, model: Model) {
-    this(new internal.VanillaLSTMBuilder(layers, inputDim, hiddenDim, model.model))
+  def this(layers: Long, inputDim: Long, hiddenDim: Long, model: Model, lnLSTM: Boolean = false) {
+    this(new internal.VanillaLSTMBuilder(layers, inputDim, hiddenDim, model.model, lnLSTM))
   }
 
   def setDropout(d: Float, dR: Float): Unit = builder.set_dropout(d, dR)
