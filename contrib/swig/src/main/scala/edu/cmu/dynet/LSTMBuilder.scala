cd7e1897c1852e0dc890de975aa19fc7c788346e
shuhei-k <shuhei-k@outlook.jp>
2017-06-09 09:00:12 +0900

    setDropout with more than 1 arguments
    Former-commit-id: 4dad1c4acd3d17f58ba4a11878b0a95dc3d89c92

--- contrib/swig/src/main/scala/edu/cmu/dynet/LSTMBuilder.scala
+++ contrib/swig/src/main/scala/edu/cmu/dynet/LSTMBuilder.scala
@@ -8,6 +8,8 @@ class VanillaLstmBuilder private[dynet](private[dynet] val builder: internal.Van
   def this(layers: Long, inputDim: Long, hiddenDim: Long, model: Model) {
     this(new internal.VanillaLSTMBuilder(layers, inputDim, hiddenDim, model.model))
   }
+
+  def setDropout(d: Float, dR: Float): Unit = builder.set_dropout(d, dR)
 }
 
 /** Builder method for creating LSTMs, as in the C++ code. For its public methods see
@@ -24,6 +26,8 @@ class LstmBuilder private[dynet](private[dynet] val builder: internal.LSTMBuilde
   def this(layers: Long, inputDim: Long, hiddenDim: Long, model: Model) {
     this(new internal.LSTMBuilder(layers, inputDim, hiddenDim, model.model))
   }
+
+  def setDropout(d: Float, dH: Float, dC: Float): Unit = builder.set_dropout(d, dH, dC)
 }
 
 
