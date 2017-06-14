c2d47b61e618ae2e1a0238f50dabe5c54299ea23
Hao Zhang <hao.zhang@petuum.com>
2017-06-13 21:15:49 -0400

    add cudnn-based gpu version of maxpooling2d
    Former-commit-id: 02af06ad2a48ba373f0e1f68bdb914ecc285e5a0

--- tests/test-nodes.cc
+++ tests/test-nodes.cc
@@ -1246,35 +1246,10 @@ BOOST_AUTO_TEST_CASE( maxpooling2d_valid_gradient ) {
   std::vector<unsigned> stride = {2, 5};
   bool is_valid = false;
   Expression w = conv2d(x, kernel, stride, is_valid);
-  //Expression z = sum_batches(sum_elems(w));
-  //BOOST_CHECK(check_grad(mod, z, 0));
   is_valid = true;
   Expression y = maxpooling2d(w, ksize, stride, is_valid);
   Expression z = sum_batches(sum_elems(y));
   BOOST_CHECK(check_grad(mod, z, 0));
-}
-
-BOOST_AUTO_TEST_CASE( maxpooling2d_valid_gradient_two ) {
-  dynet::ComputationGraph cg;
-  Parameter param_kernel = mod.add_parameters({2, 2, 1, 1});
-  std::vector<float> param_kernel_vals = {.011f, .022f, .012f, .022f};
-  TensorTools::set_elements(param_kernel.get()->values, param_kernel_vals);
-  std::vector<float> maxpooling2d_batch_vals(1 * 41 * 51 * 2);
-  for (unsigned i = 0; i < maxpooling2d_batch_vals.size(); ++i) {
-    maxpooling2d_batch_vals[i] = i * 0.011f + (i+1) * 0.001f;
-  }
-  Expression x = input(cg, Dim({41, 51, 1}, 2), maxpooling2d_batch_vals);
-  Expression kernel = parameter(cg, param_kernel);
-  std::vector<unsigned> ksize = {2, 3};
-  std::vector<unsigned> stride = {7, 5};
-  bool is_valid = false;
-  Expression w = conv2d(x, kernel, stride, is_valid);
-  Expression z = sum_batches(sum_elems(w));
-  BOOST_CHECK(check_grad(mod, z, 0));
-  is_valid = false;
-  //Expression y = maxpool(w, ksize, stride, is_valid);
-  //Expression z = sum_batches(sum_elems(y));
-  //BOOST_CHECK(check_grad(mod, z, 0));
 }
 
 BOOST_AUTO_TEST_CASE( maxpooling2d_same_gradient_two ) {
@@ -1292,8 +1267,6 @@ BOOST_AUTO_TEST_CASE( maxpooling2d_same_gradient_two ) {
   std::vector<unsigned> stride = {3, 3};
   bool is_valid = false;
   Expression w = conv2d(x, kernel, stride, is_valid);
-  //Expression z = sum_batches(sum_elems(w));
-  //BOOST_CHECK(check_grad(mod, z, 0));
   is_valid = true;
   Expression y = maxpooling2d(w, ksize, stride, is_valid);
   Expression z = sum_batches(sum_elems(y));
