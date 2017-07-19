e62bc6a5599d164eddefe601c83ee862169506c6
John Hewitt <jhewitt@qualtrics.com>
2017-07-19 15:18:20 -0600

    add even dim requirement and dimension splitting to BiRNN doc

--- python/_dynet.pyx
+++ python/_dynet.pyx
@@ -4302,7 +4302,7 @@ class BiRNNBuilder(object): # {{{
         """Args:
             num_layers: depth of the BiRNN
             input_dim: size of the inputs
-            hidden_dim: size of the outputs (and intermediate layer representations)
+            hidden_dim: size of the outputs (and intermediate layer representations.) This hidden dim is split evenly between the two constituent RNNs, and thus must be even. 
             model
             rnn_builder_factory: RNNBuilder subclass, e.g. LSTMBuilder
             builder_layers: list of (forward, backward) pairs of RNNBuilder instances to directly initialize layers
