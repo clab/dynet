92a0b711c76542c40ea19422e291486436175fd8
Matthias Sperber <matthias.sperber@gmail.com>
2017-07-21 18:57:39 +0200

    fix missing CwiseSum

--- dynet/nodes-arith-cwise.cc
+++ dynet/nodes-arith-cwise.cc
@@ -6,6 +6,93 @@ using namespace std;
 
 namespace dynet {
 
+// ************* CwiseSum*************
+#ifndef __CUDACC__
+
+string CwiseSum::as_string(const vector<string>& arg_names) const {
+  ostringstream s;
+  s << arg_names[0];
+  for (unsigned i = 1; i < arg_names.size(); ++i)
+    s << " + " << arg_names[i];
+  return s.str();
+}
+
+Dim CwiseSum::dim_forward(const vector<Dim>& xs) const {
+  DYNET_ARG_CHECK(xs.size() == 2, "Failed input count check in CwiseSum")
+  Dim d = xs[1];
+  DYNET_ARG_CHECK(xs[0].nd == xs[1].nd || xs[0].batch_size()==1 || xs[1].batch_size()==1, "CwiseSum: arguments must have equal number of dimensions, or have a scalar as one of its arguments.");
+  for(int i=0; i<xs[0].nd; i++)
+    DYNET_ARG_CHECK(xs[0].d[i]==xs[1].d[i] || xs[0].d[i]==1, "CwiseSum: For each dimension, the dim size needs to match or equal 1.");
+  DYNET_ARG_CHECK(xs[0].bd==xs[1].bd || xs[0].bd==1, "CwiseSum: batch size must match or equal 1");
+  return d;
+}
+
+#endif
+
+
+template<class MyDevice>
+void CwiseSum::forward_dev_impl(const MyDevice & dev, const vector<const Tensor*>& xs, Tensor& fx) const {
+  // convention: 1st argument will be broadcasted (expr.cc code should take care of passing in the right order)
+  DYNET_ASSERT(num_args == 2, "Bad number of arguments in CwiseSum::forward");
+  bool all_dims_same = xs[0]->d.size() == xs[1]->d.size();
+  if (all_dims_same){
+    fx.tvec().device(*dev.edevice) = xs[0]->tvec() + xs[1]->tvec();
+  } else {
+    Eigen::array<int, 5> bcast = {1,1,1,1,1};
+    for(int i=0; i<xs[0]->d.nd; i++){
+      if(xs[0]->d[i]==1) bcast[i] = xs[1]->d[i];
+    }
+    if(xs[0]->d.bd == 1) bcast[4] = xs[1]->d.bd;
+    fx.tb<4>().device(*dev.edevice) = xs[0]->tb<4>().broadcast(bcast) + xs[1]->tb<4>();
+  }
+}
+
+template<class MyDevice>
+void CwiseSum::backward_dev_impl(const MyDevice & dev,
+                             const vector<const Tensor*>& xs,
+                             const Tensor& fx,
+                             const Tensor& dEdf,
+                             unsigned i,
+                             Tensor& dEdxi) const {
+  if(i==1 || xs[0]->d.size() == xs[1]->d.size()) {
+    dEdxi.tvec().device(*dev.edevice) += dEdf.tvec();
+  } else {
+    int n_red = xs[0]->d.bd!=xs[1]->d.bd?1:0;
+    for(int i=0;i<xs[0]->d.nd; i++) if(xs[0]->d[i]!=xs[1]->d[i]) n_red++;
+    DYNET_ASSERT(n_red < 5, "Unsupported number of reductions check in CwiseSum::backward (cadd)");
+    if(n_red==1)      backward_helper<MyDevice, 1>(dev, xs, fx, dEdf, i, dEdxi);
+    else if(n_red==2) backward_helper<MyDevice, 2>(dev, xs, fx, dEdf, i, dEdxi);
+    else if(n_red==3) backward_helper<MyDevice, 3>(dev, xs, fx, dEdf, i, dEdxi);
+    else if(n_red==4) backward_helper<MyDevice, 4>(dev, xs, fx, dEdf, i, dEdxi);
+  }
+}
+DYNET_NODE_INST_DEV_IMPL(CwiseSum)
+
+template<class MyDevice, int ReductionOrder>
+void CwiseSum::backward_helper(const MyDevice & dev,
+		     const std::vector<const Tensor*>& xs,
+		     const Tensor& fx,
+		     const Tensor& dEdf,
+		     unsigned i,
+		     Tensor& dEdxi) const {
+  Eigen::array<int, ReductionOrder> red_axis;
+  red_axis[ReductionOrder-1] = 4;
+  int curr_red_axis = 0;
+  for(int di=0;di<xs[0]->d.nd; di++){
+    if(xs[0]->d[di]!=xs[1]->d[di]){
+      red_axis[curr_red_axis] = di;
+      curr_red_axis++;
+    }
+  }
+  Eigen::array<int, 5> morph = {1,1,1,1,1};
+  for(int di=0; di<xs[0]->d.nd; di++){
+    morph[di] = xs[i]->d[di];
+  }
+  morph[4] = xs[i]->d.bd;
+
+  dEdxi.tb<4>().device(*dev.edevice) += dEdf.tb<4>().sum(red_axis).reshape(morph);
+}
+
 // ************* CwiseMultiply *************
 
 #ifndef __CUDACC__
@@ -84,6 +171,7 @@ void CwiseMultiply::backward_dev_impl(const MyDevice & dev,
     else if(n_red==4) backward_helper<MyDevice, 4>(dev, xs, fx, dEdf, i, dEdxi);
   }
 }
+DYNET_NODE_INST_DEV_IMPL(CwiseMultiply)
 
 template<class MyDevice, int ReductionOrder>
 void CwiseMultiply::backward_helper(const MyDevice & dev,
