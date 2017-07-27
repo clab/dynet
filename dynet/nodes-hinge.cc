8e1d83d589b39013ad6de7af80c59159234b5366
Graham Neubig <neubig@gmail.com>
2017-07-27 17:04:00 +0100

    Some refactoring of hinge

--- dynet/nodes-hinge.cc
+++ dynet/nodes-hinge.cc
@@ -18,11 +18,14 @@ string Hinge::as_string(const vector<string>& arg_names) const {
 
 Dim Hinge::dim_forward(const vector<Dim>& xs) const {
   DYNET_ARG_CHECK(xs.size() == 1 && LooksLikeVector(xs[0]), "Bad input dimensions in Hinge: " << xs);
+  // TODO: This const_cast is ugly, but necessary(?). Perhaps we should refactor this.
+  const_cast<size_t&>(input_size) = xs[0].size();
   return Dim({1}, xs[0].bd);
 }
 
 size_t Hinge::aux_storage_size() const {
-  return dim.size() * sizeof(float);
+  DYNET_ASSERT(input_size != 0, "We should not have an input size of zero in hinge aux_storage_size()");
+  return input_size * sizeof(float);
 }
 
 #endif
@@ -30,16 +33,15 @@ size_t Hinge::aux_storage_size() const {
 template<class MyDevice>
 void Hinge::forward_dev_impl(const MyDevice & dev, const vector<const Tensor*>& xs, Tensor& fx) const {
   DYNET_ASSERT(xs.size() == 1, "Failed dimension check in Hinge::forward");
+  DYNET_ARG_CHECK(margin >= 0, "Hinge loss does not support negative margins (got " << margin << ")");
   Tensor eloss(xs[0]->d, static_cast<float*>(aux_mem), fx.device, DeviceMempool::FXS);
-  // TODO: Can we do this on device?
+  Eigen::array<int, 1> bcasts = {(int)xs[0]->d.rows()};
   if(pelement != nullptr) {
     DYNET_ARG_CHECK(fx.d.bd == 1, 
                             "Hinge was passed a single index but the corresponding expression has multiple mini-batch elements (" << fx.d.bd << ")");
     DYNET_ARG_CHECK(*pelement < xs[0]->d[0], "Index " << *pelement << " is out of bounds for hinge loss over tensor of size " << xs[0]->d);
-    const real mlystar = margin - TensorTools::access_element(*xs[0], *pelement);
-    eloss.tvec().device(*dev.edevice) = (xs[0]->tvec() + mlystar).cwiseMax(0.f);
+    eloss.tvec().device(*dev.edevice) = (xs[0]->tvec() - xs[0]->t<2>().chip<0>(*pelement).broadcast(bcasts) + margin).cwiseMax(0.f);
     TensorTools::set_element(eloss, *pelement, 0.f);
-    fx.t<0>().device(*dev.edevice) = eloss.tvec().sum();
   } else {
     DYNET_ASSERT(pelements != nullptr, "Hinge::forward has neither pointer to single element nor vector");
     DYNET_ARG_CHECK(xs[0]->d.bd == pelements->size(),
@@ -48,12 +50,12 @@ void Hinge::forward_dev_impl(const MyDevice & dev, const vector<const Tensor*>& 
     size_t batch_size = xs[0]->d.batch_size();
     for(size_t b = 0; b < fx.d.bd; b++) {
       DYNET_ARG_CHECK((*pelements)[b] < xs[0]->d[0], "Index for batch " << b << " is " << (*pelements)[b] << ", which is out of bounds for hinge loss over tensor of size " << xs[0]->d);
-      const real mlystar = margin - TensorTools::access_element(*xs[0], b*batch_size + (*pelements)[b]);
-      eloss.tb<1>().chip<1>(b).device(*dev.edevice) = (xs[0]->tb<1>().chip<1>(b) + mlystar).cwiseMax(0.f);
+      eloss.tb<1>().chip<1>(b).device(*dev.edevice) = (xs[0]->tb<1>().chip<1>(b) - xs[0]->tb<2>().chip<2>(b).chip<0>((*pelements)[b]).broadcast(bcasts) + margin).cwiseMax(0.f);
       TensorTools::set_element(eloss, b*batch_size + (*pelements)[b], 0.f);
-      fx.tb<0>().chip<0>(b).device(*dev.edevice) = eloss.tb<1>().chip<1>(b).sum();
     }
   }
+  Eigen::array<ptrdiff_t, 1> red_axis = {0};
+  fx.tb<0>().device(*dev.edevice) = eloss.tb<1>().sum(red_axis);
 }
 
 template<class MyDevice>
@@ -64,6 +66,7 @@ void Hinge::backward_dev_impl(const MyDevice & dev,
                              unsigned i,
                              Tensor& dEdxi) const {
   DYNET_ASSERT(i == 0, "Failed dimension check in Hinge::backward");
+  // TODO: Can we do this on device?
   if(pelement != nullptr) {
     if(as_scalar(fx)) { // there was some loss
       const float d = as_scalar(dEdf);
@@ -82,7 +85,6 @@ void Hinge::backward_dev_impl(const MyDevice & dev,
     Tensor eloss(xs[0]->d, static_cast<float*>(aux_mem), fx.device, DeviceMempool::FXS);
     for(size_t b = 0; b < fx.d.bd; b++) {
       if(fx_vec[b]) { // there was some loss
-        // TODO: The > comparison should not be calculated twice. Keep it in auxiliary memory?
         dEdxi.tb<1>().chip<1>(b).device(*dev.edevice) += (eloss.tb<1>().chip<1>(b) > 0.f).cast<float>() * d_vec[b];
 #if defined(__CUDACC__) && defined(EIGEN_NO_MALLOC)
         DYNET_RUNTIME_ERR("CUDA memory allocation in hinge");
--- dynet/nodes-hinge.h
+++ dynet/nodes-hinge.h
@@ -9,10 +9,10 @@ namespace dynet {
 // Let x be a std::vector-valued input, x_i represents the score of the ith element, then
 // y = \sum{i != element} max{0, margin - x_element + x_i}
 struct Hinge : public Node {
-  explicit Hinge(const std::initializer_list<VariableIndex>& a, unsigned e, real m = 1.0) : Node(a), element(e), pelement(&element), margin(m) {}
-  explicit Hinge(const std::initializer_list<VariableIndex>& a, const unsigned* pe, real m = 1.0) : Node(a), element(), pelement(pe), margin(m) {}
-  explicit Hinge(const std::initializer_list<VariableIndex>& a, const std::vector<unsigned>& e, real m = 1.0) : Node(a), element(), pelement(), elements(e), pelements(&elements), margin(m) {}
-  explicit Hinge(const std::initializer_list<VariableIndex>& a, const std::vector<unsigned>* pe, real m = 1.0) : Node(a), element(), pelement(), elements(), pelements(pe), margin(m) {}
+  explicit Hinge(const std::initializer_list<VariableIndex>& a, unsigned e, real m = 1.0) : Node(a), element(e), pelement(&element), margin(m), input_size(0) {}
+  explicit Hinge(const std::initializer_list<VariableIndex>& a, const unsigned* pe, real m = 1.0) : Node(a), element(), pelement(pe), margin(m), input_size(0) {}
+  explicit Hinge(const std::initializer_list<VariableIndex>& a, const std::vector<unsigned>& e, real m = 1.0) : Node(a), element(), pelement(), elements(e), pelements(&elements), margin(m), input_size(0) {}
+  explicit Hinge(const std::initializer_list<VariableIndex>& a, const std::vector<unsigned>* pe, real m = 1.0) : Node(a), element(), pelement(), elements(), pelements(pe), margin(m), input_size(0) {}
   virtual bool supports_multibatch() const override { return true; }
   DYNET_NODE_DEFINE_DEV_IMPL()
   size_t aux_storage_size() const override;
@@ -21,7 +21,20 @@ struct Hinge : public Node {
   std::vector<unsigned> elements;
   const std::vector<unsigned>* pelements;
   real margin;
+  size_t input_size;
 };
+
+// // x can be either a matrix (or a batch of vectors where the batch size is equal to the vector size).
+// // This will calculate a hinge loss in both directions: 
+// // y = \sum_i \sum_{j != i} max{0, margin - x_i,i + x_i,j} + max{0, margin - x_j,j + x_i,j}
+// struct BidirectionalHinge : public Node {
+//   explicit BidirectionalHinge(const std::initializer_list<VariableIndex>& a, real m = 1.0) : Node(a), margin(m), input_size(0) {}
+//   virtual bool supports_multibatch() const override { return true; }
+//   DYNET_NODE_DEFINE_DEV_IMPL()
+//   size_t aux_storage_size() const override;
+//   real margin;
+//   size_t input_size;
+// };
 
 } // namespace dynet
 
