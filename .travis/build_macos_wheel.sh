60beccafe12d3ca4385260f4331fb69280584be3
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-09-04 16:02:42 +0300

    Set LD_LIBRARY_PATH for delocate to work on macOS

--- .travis/build_macos_wheel.sh
+++ .travis/build_macos_wheel.sh
@@ -5,5 +5,6 @@ cd "$TRAVIS_BUILD_DIR"
 source activate "$PYVER"
 pip install pypandoc delocate
 python setup.py bdist_wheel
+export LD_LIBRARY_PATH="$TRAVIS_BUILD_DIR/miniconda/envs/$PYVER/lib"
 delocate-wheel -v dist/*.whl
 
