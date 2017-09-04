e8a8815e1bc056e54a916b8f5cc077cfcc533ea8
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2017-09-04 08:09:41 +0300

    Test on Docker too when arch is i686

--- .travis/build_manylinux_wheel.sh
+++ .travis/build_manylinux_wheel.sh
@@ -7,13 +7,17 @@ set -xe
 export PATH="$PWD/miniconda/bin:$PATH" CMAKE=cmake28 EIGEN3_INCLUDE_DIR="$TRAVIS_BUILD_DIR/eigen"
 cd "$TRAVIS_BUILD_DIR"
 source activate "$PYVER"
-pip install pypandoc
-python setup.py bdist_wheel
-source deactivate
-export LD_LIBRARY_PATH="$TRAVIS_BUILD_DIR/miniconda/envs/$PYVER/lib" 
-auditwheel repair dist/*.whl
-rm -f dist/*.whl
-mv -f wheelhouse/*.whl dist/
-chmod -R a+rw . "$TRAVIS_BUILD_DIR/miniconda/envs/$PYVER"
-find . "$TRAVIS_BUILD_DIR/miniconda/envs/$PYVER" -type d -exec chmod a+x {} \;
+if [[ -n "$DYNET_TEST" ]]; then
+  "$TRAVIS_BUILD_DIR"/.travis/test_dynet.sh
+else  # build
+  pip install pypandoc  # needed to generate the description from the readme
+  python setup.py bdist_wheel
+  source deactivate  # auditwheel installed in system but not in conda env
+  export LD_LIBRARY_PATH="$TRAVIS_BUILD_DIR/miniconda/envs/$PYVER/lib" 
+  auditwheel repair dist/*.whl
+  rm -f dist/*.whl
+  mv -f wheelhouse/*.whl dist/
+  chmod -R a+rw . "$TRAVIS_BUILD_DIR/miniconda/envs/$PYVER"
+  find . "$TRAVIS_BUILD_DIR/miniconda/envs/$PYVER" -type d -exec chmod a+x {} \;
+fi
 
