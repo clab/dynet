0ee93e5b4168b7658e57771ea83c0232b2cfd44b
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-09-06 09:32:53 +0300

    Avoid deploying wheel for macOS for now

--- .travis/build_macos_wheel.sh
+++ .travis/build_macos_wheel.sh
@@ -5,7 +5,15 @@ cd "$TRAVIS_BUILD_DIR"
 source activate "$PYVER"
 pip install pypandoc delocate
 python setup.py bdist_wheel
-cp "$TRAVIS_BUILD_DIR"/miniconda/envs/$PYVER/lib/libdynet.dylib ./
-delocate-listdeps --depending dist/*.whl
-delocate-wheel -v dist/*.whl
+DYLIB="$TRAVIS_BUILD_DIR"/miniconda/envs/"$PYVER"/lib/libdynet.dylib
+WHEEL=dist/*.whl
+echo Trying to relink $DYLIB into $WHEEL...
+DEPS_BEFORE="$(delocate-listdeps $WHEEL)"
+delocate-wheel -v "$WHEEL"
+DEPS_AFTER="$(delocate-listdeps $WHEEL)"
+if [[ "$DEPS_BEFORE" == "$DEPS_AFTER" ]]; then
+  echo "Failed fixing wheel, see https://github.com/clab/dynet/pull/841"
+else
+  echo "Success!"
+fi
 
