b548c0450e3305ff88a5c01de5f7c31a351031af
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2017-09-25 21:51:46 +0300

    Compile with CUDA on Travis CI (#911)
    * Disable warnings, as log is too big for Travis CI
    
    * Test on Travis CI with CUDA
    
    Build DyNet with -DBACKEND=cuda. Exclude on osx.
    
    On Travis CI, force tests to run on CPU (no GPU)
    
    * Disable tests when compiling with CUDA on Travis for now
    
    
    Former-commit-id: ae79d2e7c033e0976f930594adf5ffd17c2ac340

--- tests/test-tensor.cc
+++ tests/test-tensor.cc
@@ -5,6 +5,7 @@
 #include <dynet/expr.h>
 #include <dynet/grad-check.h>
 #include <boost/test/unit_test.hpp>
+#include "test.h"
 #include <stdexcept>
 
 using namespace dynet;
@@ -18,6 +19,7 @@ struct TensorTest {
       for (auto x : {"TensorTest", "--dynet-mem", "10"}) {
         av.push_back(strdup(x));
       }
+      ADD_EXTRA_ARGUMENTS(av)
       char **argv = &av[0];
       int argc = av.size();
       dynet::initialize(argc, argv);
