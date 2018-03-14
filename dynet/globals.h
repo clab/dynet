366e0d23d6ef3fb1629f586d098db0e8f6ae3a79
Hong Wu <xunzhang@apache.org>
2018-03-14 11:51:09 -0400

    clean up global curandeng.

--- dynet/globals.h
+++ dynet/globals.h
@@ -3,11 +3,6 @@
 
 #include <random>
 
-#ifdef HAVE_CUDA
-struct curandGenerator_st;
-typedef struct curandGenerator_st *curandGenerator_t;
-#endif
-
 namespace dynet {
 
 class Device;
@@ -16,10 +11,6 @@ class NamedTimer;
 extern std::mt19937* rndeng;
 extern Device* default_device;
 extern NamedTimer timer; // debug timing in executors.
-
-#ifdef HAVE_CUDA
-extern curandGenerator_t curandeng;
-#endif
 
 } // namespace dynet
 
