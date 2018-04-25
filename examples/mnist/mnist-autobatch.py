4f467360389857506513f191b7d4238a4656ebd0
Arne Köhn <arne@chark.eu>
2018-04-25 13:07:41 +0200

    Enhance mnist-autobatch: automatic downloads, python3, shebang (#1359)

--- examples/mnist/mnist-autobatch.py
+++ examples/mnist/mnist-autobatch.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 import time
 import random
 import os
@@ -7,9 +9,11 @@ import numpy as np
 import dynet as dy
 
 # To run this, download the four files from  http://yann.lecun.com/exdb/mnist/
-# and gunzip them into a single path. Pass this path to the program with the
-# --path option. You will also want to run with --dynet_autobatch=1.
-# To turn on GPU training, run with --dynet_gpus=1.
+# using the --download option.
+# Pass the path where the data should be stored (or is already stored)
+# to the program with the --path option. You will also want to run
+# with --dynet_autobatch=1.  To turn on GPU training, run with
+# --dynet_gpus=1.
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--path", default=".",
@@ -17,6 +21,8 @@ parser.add_argument("--path", default=".",
 parser.add_argument("--minibatch_size", default=16,
                     help="Size of minibatches.")
 parser.add_argument("--conv", dest="conv", action="store_true")
+parser.add_argument("--download", dest="download", action="store_true",
+                    help="download and extract examples to path")
 parser.add_argument("--dynet_autobatch", default=0,
                     help="Set to 1 to turn on autobatching.")
 parser.add_argument("--dynet_gpus", default=0,
@@ -34,7 +40,7 @@ def read_mnist(dataset, path):
         fname_img = os.path.join(path, "t10k-images-idx3-ubyte")
         fname_lbl = os.path.join(path, "t10k-labels-idx1-ubyte")
     else:
-        raise ValueError, "dataset must be 'testing' or 'training'"
+        raise ValueError("dataset must be 'testing' or 'training'")
 
     # Load everything in some numpy arrays
     with open(fname_lbl, "rb") as flbl:
@@ -50,7 +56,7 @@ def read_mnist(dataset, path):
     get_instance = lambda idx: (labels[idx], images[idx])
 
     # Create an iterator which returns each image in turn
-    for i in xrange(len(labels)):
+    for i in range(len(labels)):
         yield get_instance(i)
 
 
@@ -83,8 +89,24 @@ class MNISTClassify(object):
     logits = self.W2 * h
     return logits
 
+
+def download_examples(path):
+    import gzip
+    import urllib.request
+    baseurl = "http://yann.lecun.com/exdb/mnist/"
+    for elem in ["train-images-idx3-ubyte.gz",
+                 "train-labels-idx1-ubyte.gz",
+                 "t10k-images-idx3-ubyte.gz",
+                 "t10k-labels-idx1-ubyte.gz"]:
+        print("downloading " + elem + " ...")
+        outfile = open(os.path.join(path, elem[:-3]), "wb")
+        downloaded = urllib.request.urlopen(baseurl + elem)
+        outfile.write(gzip.GzipFile(fileobj = downloaded).read())
+    
 if __name__ == "__main__":
   args = parser.parse_args()
+  if args.download:
+      download_examples(args.path)
   training = [(lbl, img) for (lbl, img) in read_mnist("training", args.path)]
   testing = [(lbl, img) for (lbl, img) in read_mnist("testing", args.path)]
 
@@ -98,9 +120,9 @@ if __name__ == "__main__":
   dev_time = 0
   report = args.minibatch_size * 30
   dev_report = args.minibatch_size * 600
-  for epoch in xrange(50):
+  for epoch in range(50):
     random.shuffle(training)
-    print("Epoch {} starting".format(epoch+1))
+    print(("Epoch {} starting".format(epoch+1)))
     i = 0
     while i < len(training):
       dy.renew_cg()
@@ -124,7 +146,7 @@ if __name__ == "__main__":
 
       # Do dev evaluation here:
       if (i > 0) and (i % dev_report == 0):
-        confusion = [[0 for _ in xrange(10)] for _ in xrange(10)]
+        confusion = [[0 for _ in range(10)] for _ in range(10)]
         correct = 0
         dev_start = time.time()
         for s in range(0, len(testing), args.minibatch_size):
@@ -149,18 +171,18 @@ if __name__ == "__main__":
         dev_end = time.time()
         acc = float(correct) / len(testing)
         dev_time += dev_end - dev_start
-        print("Held out accuracy {} ({} instances/sec)".format(
-            acc, len(testing) / (dev_end - dev_start)))
-        print '   ' + ''.join(('T'+str(x)).ljust(6) for x in xrange(10))
+        print(("Held out accuracy {} ({} instances/sec)".format(
+            acc, len(testing) / (dev_end - dev_start))))
+        print('   ' + ''.join(('T'+str(x)).ljust(6) for x in range(10)))
         for p, row in enumerate(confusion):
           s = 'P' + str(p) + ' '
           s += ''.join(str(col).ljust(6) for col in row)
           print(s)
 
       if (i > 0) and (i % report == 0):
-        print("moving avg loss: {}".format(eloss))
+        print(("moving avg loss: {}".format(eloss)))
       i += mbsize
     end = time.time()
-    print("instances per sec: {}".format(
-        (i + epoch * len(training)) / (end - start - dev_time)))
+    print(("instances per sec: {}".format(
+        (i + epoch * len(training)) / (end - start - dev_time))))
 
