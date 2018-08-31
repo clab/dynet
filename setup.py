22ae0c5b8314e0c22ed8757445efe068aa7e643c
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-08-31 16:16:54 +0900

    Fix #1435: build wheels for Python 3.7

--- setup.py
+++ setup.py
@@ -389,6 +389,7 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     author="Graham Neubig",
