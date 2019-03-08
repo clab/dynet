28eaf94d79f985eb1d3aa3f5798246332f1d9f11
austinma <austinma@cs.cmu.edu>
2019-03-08 12:20:58 -0500

    Revert "use more recent eigen"
    This reverts commit 096956560280a1ae78037a0337e209302513515e.

--- .travis/install_dependencies.sh
+++ .travis/install_dependencies.sh
@@ -47,7 +47,7 @@ if [[ "$BACKEND" == cuda ]]; then
 fi
 
 # Eigen
-hg clone https://bitbucket.org/eigen/eigen/ -r 9c30033
+hg clone https://bitbucket.org/eigen/eigen/ -r b2e267d
 cd eigen
 mkdir build && cd build
 cmake ..
