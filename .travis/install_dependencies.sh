557343455d9cc22227960c219d06611e32cca8d7
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2020-01-17 13:57:17 +0100

    Fix fatal error: openssl/opensslv.h: No such file or directory
    https://stackoverflow.com/a/46008642/223267

--- .travis/install_dependencies.sh
+++ .travis/install_dependencies.sh
@@ -5,7 +5,7 @@ set -xe
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
   sudo apt-get -qq update
-  sudo apt-get install -y gcc-4.8 g++-4.8
+  sudo apt-get install -y gcc-4.8 g++-4.8 libssl-dev
   PYTHON_PACKAGES="numpy pypandoc twine auditwheel cython"
   if [[ "$PYTHON_INSTALL" == manual ]]; then
     sudo apt-get install -y --allow-unauthenticated libboost-filesystem1.55-dev libboost-program-options1.55-dev libboost-serialization1.55-dev libboost-test1.55-dev libboost-regex1.55-dev
